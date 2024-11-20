f5060aff91e2d1984f4deaf3bf99fbaaa952d88c
Greg Clinton <greg.clinton@gmail.com>
2024-11-20 13:16:03 -0800

    latest

--- rag.py
+++ rag.py
@@ -19,13 +19,14 @@ docs = [WebBaseLoader(url).load() for url in urls]
 docs_list = [item for sublist in docs for item in sublist]
 
 text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
-    chunk_size=100, chunk_overlap=50
+    chunk_size = 100, chunk_overlap = 50
 )
 doc_splits = text_splitter.split_documents(docs_list)
 
 vectorstore = Chroma.from_documents(
-    documents=doc_splits,
-    collection_name="rag-chroma",
-    embedding=OpenAIEmbeddings(),
+    documents = doc_splits,
+    collection_name = "rag-chroma",
+    embedding = OpenAIEmbeddings(),
 )
+
 retriever = vectorstore.as_retriever()
