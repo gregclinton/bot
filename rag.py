4a8fbaad632646e1c31b2aa4629b6e55d7c8270b
Greg Clinton <greg.clinton@gmail.com>
2024-11-20 13:20:46 -0800

    latest

--- rag.py
+++ rag.py
@@ -30,3 +30,13 @@ vectorstore = Chroma.from_documents(
 )
 
 retriever = vectorstore.as_retriever()
+
+from langchain.tools.retriever import create_retriever_tool
+
+retriever_tool = create_retriever_tool(
+    retriever,
+    "retrieve_blog_posts",
+    "Search and return information about Lilian Weng blog posts on LLM agents, prompt engineering, and adversarial attacks on LLMs.",
+)
+
+tools = [retriever_tool]
