561881ce3d1dc2493785c67b6b2ef727b949c8f9
Greg Clinton <greg.clinton@gmail.com>
2024-11-20 14:04:50 -0800

    latest

--- rag.py
+++ rag.py
@@ -1,54 +1,44 @@
 # https://langchain-ai.github.io/langgraph/tutorials/rag/langgraph_agentic_rag/#retriever
 
-import os
-os.environ["USER_AGENT"] = "simon"
-from langchain_community.document_loaders import WebBaseLoader
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.tools.retriever import create_retriever_tool
 from dotenv import load_dotenv
 
 load_dotenv('keys')
 
 def create_db():
-    urls = [
+    import os
+    os.environ["USER_AGENT"] = "simon"
+    from langchain_community.document_loaders import WebBaseLoader
+    from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+    docs = [WebBaseLoader(url).load() for url in [
         "https://lilianweng.github.io/posts/2023-06-23-agent/",
         "https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/",
         "https://lilianweng.github.io/posts/2023-10-25-adv-attack-llm/",
-    ]
+    ]]
 
-    docs = [WebBaseLoader(url).load() for url in urls]
-
-    docs_list = [item for sublist in docs for item in sublist]
-
-    text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    splits = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
         chunk_size = 100, chunk_overlap = 50
-    )
-    doc_splits = text_splitter.split_documents(docs_list)
+    ).split_documents([item for sublist in docs for item in sublist])
 
-    vectorstore = Chroma.from_documents(
-        documents=doc_splits,
-        collection_name="rag-chroma",
-        embedding=OpenAIEmbeddings(),
-        persist_directory='storage'
+    Chroma.from_documents(
+        documents = splits,
+        collection_name = "rag-chroma",
+        embedding = OpenAIEmbeddings(),
+        persist_directory = 'storage'
     )
 
 # create_db()
 
-vectorstore = Chroma(
-    collection_name="rag-chroma",
-    embedding_function=OpenAIEmbeddings(),
-    persist_directory='storage'
-)
-
-retriever = vectorstore.as_retriever()
-
-from langchain.tools.retriever import create_retriever_tool
-
-retriever_tool = create_retriever_tool(
-    retriever,
+retriever = create_retriever_tool(Chroma(
+        collection_name = "rag-chroma",
+        embedding_function = OpenAIEmbeddings(),
+        persist_directory = 'storage'
+    ).as_retriever(),
     "retrieve_blog_posts",
     "Search and return information about Lilian Weng blog posts on LLM agents, prompt engineering, and adversarial attacks on LLMs.",
 )
 
-tools = [retriever_tool]
+tools = [retriever]
