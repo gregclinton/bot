190d89e81cd039109ca6a11678ba70c845c2ac3a
Greg Clinton <greg.clinton@gmail.com>
2024-11-21 09:34:58 -0800

    latest

--- chroma.py
+++ chroma.py
@@ -9,16 +9,14 @@ embedding = OpenAIEmbeddings()
 
 def store(collection_name, text):
     from langchain.schema import Document
+    from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-    if False:
-        from langchain_text_splitters import RecursiveCharacterTextSplitter
-
-        splits = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
-            chunk_size = 100, chunk_overlap = 50
-        ).split_documents([item for sublist in [Document(text)] for item in sublist])
+    splits = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size = 100, chunk_overlap = 50
+    ).split_documents([Document(text)])
 
     Chroma.from_documents(
-        documents = [Document(text)],
+        documents = splits,
         collection_name = collection_name,
         embedding = embedding,
         persist_directory = f"chroma/{collection_name}")
