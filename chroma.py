7a95b04884b0d7c3e70e8000f54fd8709fa9510a
Greg Clinton <greg.clinton@gmail.com>
2024-11-21 08:04:36 -0800

    latest

--- chroma.py
+++ chroma.py
@@ -6,9 +6,11 @@ from langchain.tools.retriever import create_retriever_tool
 
 embedding = OpenAIEmbeddings()
 
-def store(collection_name, documents):
+def store(collection_name, text):
+    from langchain.schema import Document
+
     Chroma.from_documents(
-        documents = documents,
+        documents = [Document(text)],
         collection_name = collection_name,
         embedding = embedding,
         persist_directory = f"chroma/{collection_name}")
@@ -21,4 +23,4 @@ def retriever(collection_name, description = ""):
             persist_directory = f"chroma/{collection_name}"
         ).as_retriever(),
         f"retrieve_{collection_name}",
-        f"Retrieve information about {collection_name}. {description}}")
+        f"Retrieve information about {collection_name}. {description}")
