6c030833f0a1798d5c8345aaf82e53e74eabe203
Greg Clinton <greg.clinton@gmail.com>
2024-11-22 08:02:28 -0800

    latest

--- play.py
+++ play.py
@@ -2,6 +2,8 @@
 # sudo docker run -v `pwd`:/root -w /root agent:latest langgraph dev
 
 from react import React
-from assistants import cast
+from assistants import cast, get
 
 print(React("Be good.", [cast]).run("Cast an admin to do command line activities."))
+
+print(get("System Administrator").run("List the files in the current working directory."))
