4c3fb6c1234916ac0842148f8686766b6db4bc60
Greg Clinton <greg.clinton@gmail.com>
2024-11-22 15:16:19 -0800

    latest

---
+++ jokes.py
@@ -0,0 +1,28 @@
+from langgraph.graph import StateGraph, MessagesState
+from langgraph.checkpoint.memory import MemorySaver
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv('keys')
+
+def call_model(state):
+    return {'messages': ChatOpenAI(model = "gpt-4o-mini").invoke(state['messages'])}
+
+workflow = StateGraph(MessagesState)
+workflow.add_node('agent', call_model)
+workflow.set_entry_point('agent')
+subgraph = workflow.compile(checkpointer = MemorySaver())
+
+workflow = StateGraph(MessagesState)
+workflow.add_node('agent', call_model)
+workflow.add_node('joker', subgraph=subgraph)
+workflow.set_entry_point('agent')
+workflow.add_edge("agent", "joker")
+graph = workflow.compile(checkpointer = MemorySaver())
+
+prompt = "flower"
+thread = {'configurable': {'thread_id': "1"},}
+for event in graph.stream({"messages": [('user', prompt)]}, thread, stream_mode = 'values'):
+    pass
+
+print(event['messages'][-1].content)
