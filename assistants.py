ea5427c33c7d091127fd30e7b882dbdf6479ea8a
Greg Clinton <greg.clinton@gmail.com>
2024-11-22 08:24:57 -0800

    latest

--- assistants.py
+++ assistants.py
@@ -8,12 +8,11 @@ assistants = {}
 def cast(job_title, instructions, tools):
     """
 Cast an AI assistant with a job title, instructions and a set of tools.
-Tools is a comma separated string of tools that may include search and/or shell.
+Tools is a comma separated string of tool names.
+These may include search, cast, chroma and/or shell.
 You must provide at least one tool.
     """
-    print(f"{job_title} {instructions} {tools}")
-    tools = list(map(lambda t : {"search": search, "shell": shell}[t], tools.split(",")))
-    assistants[job_title] = React(instructions, tools)
+    assistants[job_title] = React(instructions, list(map(lambda t : {"search": search, "shell": shell}[t], tools.split(","))))
     return "success"
 
 def get(job_title):
