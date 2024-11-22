9933bbb7501d83e04b92e0a73220a8246b8bcb90
Greg Clinton <greg.clinton@gmail.com>
2024-11-22 08:14:37 -0800

    latest

--- play.py
+++ play.py
@@ -4,6 +4,8 @@
 from react import React
 import assistants
 
-print(React("Be good.", [assistants.cast]).run("Cast an assistant with job tilte admin, all lowercase, to do command line activities."))
+print(React("Be good.", [assistants.cast]).run("""
+Cast an AI assistant with job title "admin" to do command line activities.
+"""))
 
 print(assistants.get("admin").run("List the files in the current working directory."))
