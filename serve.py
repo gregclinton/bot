e4f261dc7031f84cf6a66dc44d4a7cc7b21b9d58
Greg Clinton <greg.clinton@gmail.com>
2024-11-22 11:09:58 -0800

    latest

--- serve.py
+++ serve.py
@@ -1,13 +1,14 @@
 # sudo docker run -p 8123:8123 -v `pwd`:/root -w /root agent:latest uvicorn serve:app --host 0.0.0.0 --port 8123 --reload
 
 from react import React
+from assistants import cast, call
 import threads
 from tools import shell, search
 from fastapi import FastAPI, Request
 
 app = FastAPI()
 
-assistant = React("Do your best.", [shell, search])
+assistant = React("Do your best.", [shell, search, cast, call])
 
 @app.post('/prompts')
 async def post_prompt(req: Request):
