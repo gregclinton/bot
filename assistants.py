5c585b813a7e3270292cefc4917c3501a5aef274
Greg Clinton <greg.clinton@gmail.com>
2024-11-22 12:03:47 -0800

    latest

--- assistants.py
+++ assistants.py
@@ -1,19 +1,21 @@
 from react import React
-from tools import search, shell
 from langchain_community.tools import tool
+import tools
 
 assistants = {}
 
 @tool
-def cast(role, instructions, tools):
+def cast(role, instructions, tools_csv):
     """
 Cast an AI assistant with the specified role, instructions and a set of tools.
 Tools is a comma separated string of tool names.
 These may include search, cast, chroma and/or shell.
 You must provide at least one tool.
     """
-    assistants[role] = React(instructions, list(map(lambda t : {"search": search, "shell": shell}[t], tools.split(","))))
+    assistants[role] = React(instructions, list(map(lambda t : tools.get(t), tools_csv.split(","))))
     return "success"
+
+tools.put("cast", cast)
 
 @tool
 def call(role, prompt):
@@ -21,3 +23,5 @@ def call(role, prompt):
 Call the assistant with the specified role and prompt.
     """
     return assistants[role].run(prompt)
+
+tools.put("call", call)
--- tools.py
+++ tools.py
@@ -1,5 +1,13 @@
 from langchain_community.tools import tool, TavilySearchResults
 
+tools = {}
+
+def put(name, tool):
+    tools[name] = tool
+
+def get(name):
+    return tools[name]
+
 search = TavilySearchResults(
     max_results=2,
     search_depth="advanced",
@@ -8,10 +16,14 @@ search = TavilySearchResults(
     include_images=False
 )
 
+put("search", search)
+
 @tool
 def shell(line):
     """
         run a shell command
     """
     import subprocess
-    return subprocess.run(line, shell = True, capture_output = True, text = True).stdout
+    return subprocess.run(line, shell = True, capture_output = True, text = True)
+
+put("shell", shell)
