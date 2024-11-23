c6548ae05660977c1df8c922f1d601fa75ee6af3
Greg Clinton <greg.clinton@gmail.com>
2024-11-23 13:01:27 -0800

    latest

--- serve.py
+++ serve.py
@@ -1,18 +1,30 @@
 # sudo docker run -p 8123:8123 -v `pwd`:/root -w /root agent:latest uvicorn serve:app --host 0.0.0.0 --port 8123 --reload
 
-from react import React
-from assistants import cast, call
 import threads
 from tools import shell, search
 from fastapi import FastAPI, Request
+from langgraph.prebuilt import create_react_agent
+from langchain_openai import ChatOpenAI
+from langgraph.graph import StateGraph, MessagesState
+from langgraph.checkpoint.memory import MemorySaver
+from threads import thread
 
 app = FastAPI()
 
-assistant = React("Do your best.", [shell, search, cast, call])
+llm = ChatOpenAI(model = "gpt-4o-mini")
+builder = StateGraph(MessagesState)
+builder.add_node('agent', create_react_agent(llm, [shell, search], state_modifier="Do your best."))
+builder.set_entry_point('agent')
+graph = builder.compile(checkpointer = MemorySaver())
 
 @app.post('/prompts')
 async def post_prompt(req: Request):
-    return { "content":  assistant.run((await req.json())['prompt']) }
+    def run(prompt):
+        for event in graph.stream({"messages": [('user', prompt)]}, thread(), stream_mode = 'values'):
+            pass
+
+        return event['messages'][-1].content
+    return { "content": run((await req.json())['prompt']) }
 
 @app.delete('/thread/current')
 async def delete_thread():
