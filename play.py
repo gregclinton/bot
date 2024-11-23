337eec8378131f7c59d2da1e4db5e9cc495a62d2
Greg Clinton <greg.clinton@gmail.com>
2024-11-23 10:36:12 -0800

    latest

--- play.py
+++ play.py
@@ -18,26 +18,30 @@ from tools import shell
 class AgentState(MessagesState):
     next: str
 
-members = ["rabbi", "admin"]
 
 llm = ChatOpenAI(model = "gpt-4o-mini")
-
-def supervisor(state):
-    class Router(TypedDict): next: Literal[*(members + [END])]
-    prompt = f"From {members} pick the more appropriate. Respond {END} when either has responded."
-    return llm.with_structured_output(Router).invoke([SystemMessage(prompt)] + state["messages"])
 
 rabbi = lambda state: {"messages": [HumanMessage(content="The meaning of life is to be good.")]}
 admin = create_react_agent(llm, tools=[shell], state_modifier="You are an admin. Use the shell tool.")
 
 builder = StateGraph(AgentState)
-builder.add_node("supervisor", supervisor)
 builder.add_node("rabbi", rabbi)
 builder.add_node("admin", admin)
 
-for member in members:
-    builder.add_edge(member, "supervisor")
+def supervise(builder):
+    members = ["rabbi", "admin"]
 
+    def supervisor(state):
+        class Router(TypedDict): next: Literal[*(members + [END])]
+        prompt = f"From {members} pick the more appropriate. Respond {END} when either has responded."
+        return llm.with_structured_output(Router).invoke([SystemMessage(prompt)] + state["messages"])
+
+    builder.add_node("supervisor", supervisor)
+
+    for member in members:
+        builder.add_edge(member, "supervisor")
+
+supervise(builder)
 builder.add_conditional_edges("supervisor", lambda state: state["next"])
 builder.set_entry_point("supervisor")
 graph = builder.compile()
