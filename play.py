74eea5bf9eee55e606d71e1c973814afa4a6f48d
Greg Clinton <greg.clinton@gmail.com>
2024-11-23 07:20:32 -0800

    latest

--- play.py
+++ play.py
@@ -1,13 +1,17 @@
 # sudo docker run -v `pwd`:/root -w /root agent:latest python3 play.py
 # sudo docker run -v `pwd`:/root -w /root agent:latest langgraph dev
 
-from langgraph.graph import MessagesState
-from langchain_openai import ChatOpenAI
-from typing import Literal
-from typing_extensions import TypedDict
 from dotenv import load_dotenv
 
 load_dotenv('keys')
+
+from langgraph.graph import StateGraph, START, END, MessagesState
+from langchain_openai import ChatOpenAI
+from typing import Literal
+from typing_extensions import TypedDict
+from langchain_core.messages import HumanMessage
+from langgraph.prebuilt import create_react_agent
+from tools import search, shell
 
 class AgentState(MessagesState):
     next: str
@@ -36,3 +40,29 @@ def supervisor_node(state: AgentState) -> AgentState:
         next_ = END
 
     return {"next": next_}
+
+research_agent = create_react_agent(
+    llm, tools=[search], state_modifier="You are a researcher. DO NOT do any math."
+)
+
+def research_node(state):
+    result = research_agent.invoke(state)
+    return {
+        "messages": [
+            HumanMessage(content=result["messages"][-1].content, name="researcher")
+        ]
+    }
+
+code_agent = create_react_agent(llm, tools=[shell])
+
+def code_node(state):
+    result = code_agent.invoke(state)
+    return {
+        "messages": [HumanMessage(content=result["messages"][-1].content, name="coder")]
+    }
+
+builder = StateGraph(AgentState)
+builder.add_node("supervisor", supervisor_node)
+builder.add_node("researcher", research_node)
+builder.add_node("coder", code_node)
+builder.set_entry_point("supervisor")
