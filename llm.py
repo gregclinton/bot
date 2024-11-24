cc6bd8bc0e76e0b778d2b447b1bca3bc915fe7b8
Greg Clinton <greg.clinton@gmail.com>
2024-11-24 07:54:29 -0800

    latest

---
+++ llm.py
@@ -0,0 +1,19 @@
+# sudo docker run -v `pwd`:/root -w /root company:latest python3 mail.py
+
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv("keys")
+
+def invoke(prompt):
+    return requests.post("https://api.openai.com/v1/chat/completions", 
+    headers = {
+        'Authorization': 'Bearer ' + os.environ['OPENAI_API_KEY'],
+        'Content-Type': 'application/json',
+    },
+    json = {
+    "model": "gpt-4o-mini",
+    "messages": [{"role": "user", "content": prompt}],
+    "temperature": 0
+    }).json()["choices"][0]["message"]["content"]
--- mail.py
+++ mail.py
@@ -1,33 +1,3 @@
-# sudo docker run -v `pwd`:/root -w /root company:latest python3 mail.py
+import llm
 
-from dotenv import load_dotenv
-import os
-
-load_dotenv("keys")
-
-def setup():
-    headers = {
-        'Authorization': 'Bearer ' + os.environ['OPENAI_API_KEY'],
-        'Content-Type': 'application/json',
-    }
-
-    base_url = 'https://api.openai.com/v1'
-
-    import requests
-
-    return (
-         lambda path, data: requests.post(f'{base_url}/{path}', json = data, headers = headers).json(),
-         lambda path: requests.get(f'{base_url}/{path}', headers = headers).json()['data'],
-         lambda path: requests.delete(f'{base_url}/{path}', headers = headers)
-    )
-
-post, get, nix = setup()
-del setup
-
-res = post(f'chat/completions', {
-     "model": "gpt-4o-mini",
-     "messages": [{"role": "user", "content": "Say this is a test!"}],
-     "temperature": 0
-})
-
-print(res["choices"][0]["message"]["content"])
+print(llm.invoke("hi"))
