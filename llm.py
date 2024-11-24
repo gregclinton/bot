400ab52053c1a063e465277dfc698724b4051748
Greg Clinton <greg.clinton@gmail.com>
2024-11-24 07:55:28 -0800

    latest

--- llm.py
+++ llm.py
@@ -7,13 +7,15 @@ import os
 load_dotenv("keys")
 
 def invoke(prompt):
-    return requests.post("https://api.openai.com/v1/chat/completions", 
-    headers = {
-        'Authorization': 'Bearer ' + os.environ['OPENAI_API_KEY'],
-        'Content-Type': 'application/json',
-    },
-    json = {
-    "model": "gpt-4o-mini",
-    "messages": [{"role": "user", "content": prompt}],
-    "temperature": 0
-    }).json()["choices"][0]["message"]["content"]
+    return requests.post(
+        "https://api.openai.com/v1/chat/completions", 
+        headers = {
+            'Authorization': 'Bearer ' + os.environ['OPENAI_API_KEY'],
+            'Content-Type': 'application/json',
+        },
+        json = {
+            "model": "gpt-4o-mini",
+            "messages": [{"role": "user", "content": prompt}],
+            "temperature": 0
+        }
+    ).json()["choices"][0]["message"]["content"]
