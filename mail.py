c5c005039fb5070be6bc50814f9a19a022ab516b
Greg Clinton <greg.clinton@gmail.com>
2024-11-24 09:54:36 -0800

    latest

--- mail.py
+++ mail.py
@@ -4,4 +4,7 @@ import llm
 
 with open('mail.txt', 'r') as file:
     for line in file.readlines():
-        print(line.strip()) 
+        line = line.rstrip()
+        if line.startswith("To: "):
+            to = line.split(" ")[1]
+            print(to) 
