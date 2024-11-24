2e538247aa04cdb9e1382afd9481f52d3c903e83
Greg Clinton <greg.clinton@gmail.com>
2024-11-24 15:53:15 -0800

    latest

--- mail.py
+++ mail.py
@@ -1,34 +1,40 @@
 import llm
 
+class Email:
+    def __init__(self, sender, recipient, body, user):
+        self.sender = sender
+        self.recipient = recipient
+        self.user = user
+        self.body = body
+
+    def to_string():
+        text = "--------------------------------------------------------------------------------\n"
+        text += ("To: " + self.recipient + "\nFrom: " + self.sender + "\n")
+        if self.user:
+            text += ("Re: " + self.user + "\n")
+        text += self.body
+        return text
+
+    @staticmethod
+    def from_string(text):
+        body = ""
+
+        for line in text.split("\n"):
+            value = lambda: line.rstrip().split(" ")[1]
+            recipient, sender, user, body = ("", "", "", "")
+
+            if line.startswith("To: "):
+                recipient = value()
+            elif line.startswith("From: "):
+                sender = value()
+            elif line.startswith("Re: "):
+                user = value()
+            else:
+                body += line + "\n"
+        return Email(sender, recipient, body, user)        
+
 departments = set()
 emails = []
-
-def to_string(email):
-    text = "--------------------------------------------------------------------------------\n"
-    text += ("To: " + email["recipient"] + "\nFrom: " + email["sender"] + "\n")
-    if "user" in email:
-        text += ("Re: " + email["user"] + "\n")
-    text += email["body"]
-    return text
-
-def parse(text):
-    email = {}
-    body = ""
-
-    for line in text.split("\n"):
-        value = lambda: line.rstrip().split(" ")[1]
-
-        if line.startswith("To: "):
-            email["recipient"] = value()
-        elif line.startswith("From: "):
-            email["sender"] = value()
-        elif line.startswith("Re: "):
-            email["user"] = value()
-        else:
-            body += line + "\n"
-
-    email["body"] = body
-    return email
 
 def split_cuts(text):
     cuts = []
@@ -49,7 +55,7 @@ def split_cuts(text):
 
 with open('mail.txt', 'r') as file:
     for cut in split_cuts(file.read()):
-        emails.append(parse(cut))
+        emails.append(Email.from_string(cut))
 
 for department in ["Sales"]:
     instruction = f"You are an AI worker in {department}. "
@@ -60,23 +66,20 @@ for department in ["Sales"]:
 
     # get last email to this department
     for email in emails:
-        if email["recipient"] == department:
+        if email.recipient == department:
             last_email = email
 
     if not last_email:
-        return
-
-    is_email = lambda s : "@" in s
-    user = email["sender"] if is_email(email["sender"]) else email["user"] if "user" in email else None
+        continue
 
     # include unanswered emails to this department
     # for each unanswered email include all emails with same user
     # and always include emails to company
     # and always include emails from Management to this department
     for email in emails:
-        if email["recipient"] in [department, "company"]:
+        if email.recipient in [department, "company"]:
             prompt += to_string(email)
 
     with open('mail.txt', 'r') as file:
         for cut in split_cuts(llm.invoke(instruction, prompt)):
-            print(to_string(parse(cut)))
+            print(to_string(from_string(cut)))
