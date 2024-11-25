c900a2ed3439e9d85d16d393316d6854a6e4315e
Greg Clinton <greg.clinton@gmail.com>
2024-11-25 13:36:38 -0800

    latest

--- chat.js
+++ chat.js
@@ -5,9 +5,7 @@ const chat = {
         await fetch('/company/messages/' + account, {
             method: 'POST',
             headers:  { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                prompt: prompt
-            })
+            body: JSON.stringify({ prompt: prompt })
         })
     },
 
@@ -30,22 +28,22 @@ setInterval(() => {
             msgs.forEach(msg => {
                 const name = msg.sender === account ? 'me' : 'ai';
                 const title = document.createElement('span');
-    
+
                 title.innerHTML = name
                 title.classList.add('name');
-    
+
                 const top = document.createElement('div');
-    
+
                 top.append(title);
-    
+
                 const bottom = document.createElement('div');
                 bottom.innerHTML = msg.body;
-    
+
                 const post = document.createElement('div');
                 post.append(top, bottom);
                 post.classList.add('post');
                 document.getElementById('chat').appendChild(post);
-    
+
                 post.scrollIntoView({ behavior: 'smooth' });
             });
         })}, 500);
