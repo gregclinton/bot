250ccc6472501471d5e40ee9e9c78110cdbdcf84
Greg Clinton <greg.clinton@gmail.com>
2024-11-26 07:29:38 -0800

    latest

--- company.py
+++ company.py
@@ -8,9 +8,7 @@ import os
 company = "sephora"
 mgmt = f"{company}.txt"
 calls = f"{company}.calls.txt"
-
-no_company = lambda msg: msg.recipient != "company"
-departments = list(messages.recipients(mgmt, no_company))
+departments = list(messages.recipients(mgmt, lambda msg: msg.recipient != "company"))
 
 def invoke(account, prompt):
     messages.append_to_file(calls, [Message(account, "Sales", prompt)])
