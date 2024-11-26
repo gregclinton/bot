83b3530080fadc8c828b1d3362edc9f8c91151cc
Greg Clinton <greg.clinton@gmail.com>
2024-11-26 12:53:20 -0800

    latest

--- catalog.py
+++ catalog.py
@@ -1,12 +1,4 @@
 import llm
-
-for product in ["men's hats"]:
-    print(llm.invoke(
-        "Yor are a creative catalog writer.", 
-        f"Invent a catlog description (about 200 words) and price for {product}. Pure text format, no markdown.")
-    )
-exit()
-
 import chromadb
 from chromadb.utils import embedding_functions
 from dotenv import load_dotenv
@@ -14,22 +6,37 @@ import os
 
 load_dotenv("keys")
 
-openai_ef = embedding_functions.OpenAIEmbeddingFunction(
-    api_key=os.environ["OPENAI_API_KEY"],
-    model_name="text-embedding-ada-002"
-)
+def collection():
+    openai_ef = embedding_functions.OpenAIEmbeddingFunction(
+        api_key=os.environ["OPENAI_API_KEY"],
+        model_name="text-embedding-ada-002"
+    )
 
-client = chromadb.Client()
+    client = chromadb.Client()
 
-collection = client.create_collection(
-    name = "catalog",
-    embedding_function = openai_ef
-)
+    collection = client.create_collection(
+        name = "catalog",
+        embedding_function = openai_ef
+    )
 
-if False:
-    documents = ["Document 1 text", "Document 2 text"]
-    metadatas = [{"source": "source1"}, {"source": "source2"}]
-    ids = ["id1", "id2"]
-    collection.add(documents=documents, metadatas=metadatas, ids=ids)
+def create():
+    documents = []
+    metadatas = []
+    ids = []
+    id = 10000
 
-print(collection.query(query_texts=["Document 1"], n_results=1)["documents"][0])
+    for shoppers in ["men", "women"]:
+        instruction = "Yor are a creative catalog writer."
+        for product in ["hats", "shirts", "pants", "shoes"]:
+            prompt = f"Invent a catlog description (about 200 words) and price for {shoppers}'s {product}. Pure text format, no markdown."
+            documents.append(llm.invoke(instruction, prompt))
+            metadatas.append({"section": f"{shoppers}'s {product}"})
+            ids.append(f"id-{id}")
+            id += 1
+
+    collection().add(documents=documents, metadatas=metadatas, ids=ids)
+
+def query(query):
+    return collection().query(query_texts=[query], n_results=1)["documents"][0])
+
+print(query("I'm interested in a man's hat.")))
