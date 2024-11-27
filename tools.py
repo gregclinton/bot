69ff3a6283814df4af44e8706ddc21df4a3de98d
Greg Clinton <greg.clinton@gmail.com>
2024-11-26 18:30:43 -0800

    latest

--- tools.py
+++ tools.py
@@ -10,13 +10,13 @@ def invoke(tool, msgs):
     lookup = set()
     unanswered = []
     answers = []
+    fn = bench[tool]
 
     for msg in msgs:
         if msg.recipient == tool:
             unanswered.append(msg)
 
     for msg in unanswered:
-        if tool in bench:
-            answers.append(Message(tool, msg.sender, bench[tool](msg.body)))
+        answers.append(Message(tool, msg.sender, fn(msg.body)))
 
     return messages.to_string(answers) if answers else ""
