1b5755339a891af551a9d7bfa24d2931e47f7da6
Greg Clinton <greg.clinton@gmail.com>
2024-11-27 08:28:16 -0800

    latest

--- company.py
+++ company.py
@@ -19,13 +19,13 @@ def invoke(caller, prompt):
 
     while llm.counter < max_llm_invokes and agents:
         agent = agents.pop()
-        read = lambda path: open(f"ar/{company}/{path}", "r").read()
-        instructions = read("All").replace("{agent}", agent) + read(agent)
 
         if agent in tools.bench:
             run += messages.from_string(tools.invoke(agent, run))
             agents.add(run[-1].to_)
         else:
+            read = lambda path: open(f"ar/{company}/{path}", "r").read()
+            instructions = read("All").replace("{agent}", agent) + read(agent)
             msgs = list(filter(lambda msg: agent in (msg.from_, msg.to_), history)) + run
             completion = llm.invoke(instructions, messages.to_string(msgs))
             sanity = lambda msg: msg.from_ == agent and msg.to_ != msg.from_ and (msg.to_ != caller or msg.from_ == intake)
