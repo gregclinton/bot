b5b864043c81dedb0796c142dc081daf4cef48be
Greg Clinton <greg.clinton@gmail.com>
2024-11-27 06:32:38 -0800

    latest

--- company.py
+++ company.py
@@ -5,9 +5,8 @@ import tools
 
 def invoke(account, prompt):
     company = "sephora"
-    intake = "Sales"
-    mgmt = f"{company}.txt"
-    calls = f"{company}.calls.txt"
+    intake = "Intake"
+    calls = "messages.txt"
     departments = set()
     max_llm_invokes = 10
     llm.reset_counter()
@@ -19,14 +18,13 @@ def invoke(account, prompt):
     while llm.counter < max_llm_invokes and departments:
         department = departments.pop()
 
-        msgs = load(mgmt, lambda msg: msg.sender in ("Management") and msg.recipient in (department, "company"))
-        msgs += load(calls, lambda msg: msg.account == account and department in (msg.sender, msg.recipient))
+        read = lambda path: open(f"ar/{company}/{path}", "r").read()
+        instructions = read("All").replace("{department}", department) + read(department)
 
-        with open("instructions", "r") as file:
-            instructions = file.read().replace("{department}", department)
+        msgs = load(calls, lambda msg: msg.account == account and department in (msg.sender, msg.recipient))
 
         completion = tools.invoke(department, load(calls)) if department in tools.bench else llm.invoke(instructions, messages.to_string(msgs))
-        sanity = lambda msg: msg.sender == department and msg.recipient not in (msg.sender, "Management") and (msg.recipient != account or msg.sender == intake)
+        sanity = lambda msg: msg.sender == department and msg.recipient not in (msg.sender) and (msg.recipient != account or msg.sender == intake)
         msgs = messages.from_string(completion, sanity)
 
         print(messages.to_string(msgs))
@@ -49,5 +47,5 @@ def invoke(account, prompt):
     messages.append_to_file(calls, [msg])
     return msg.body
 
-invoke("account-375491", "Do you sell men's shoes?")
-# invoke("account-375491", "What's my balance?")
+# invoke("account-375491", "Do you sell men's shoes?")
+invoke("account-375491", "What's my balance?")
