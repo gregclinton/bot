d495cbb9eb9216c7b13497c2e1c4ed67d0b1be2b
Greg Clinton <greg.clinton@gmail.com>
2024-11-27 09:06:24 -0800

    latest

--- catalog.py
+++ catalog.py
@@ -2,7 +2,7 @@ import chroma
 import llm
 
 def invoke(query):
-    entry = chroma.collection().query(query_texts=[query], n_results=1)["documents"][0][0]
+    entry = chroma.collection("catalog").query(query_texts=[query], n_results=1)["documents"][0][0]
     context = f"A search of our product catalog yielded: \n{entry}"
     instructions = "Provide the answer to the question given the context."
     prompt = f"Context: {context}\nQuestion: {query}\nAnswer: "
--- chroma.py
+++ chroma.py
@@ -5,9 +5,9 @@ import os
 
 load_dotenv("keys")
 
-def collection():
+def collection(name):
     return chromadb.PersistentClient(path="./chroma_data").get_or_create_collection(
-        name="catalog",
+        name=name,
         embedding_function=embedding_functions.OpenAIEmbeddingFunction(
             api_key=os.environ["OPENAI_API_KEY"],
             model_name="text-embedding-ada-002"
@@ -17,19 +17,19 @@ def collection():
 if False:
     import llm
 
-    def create():
-        documents = []
-        metadatas = []
-        ids = []
-        id = 10000
+    documents = []
+    metadatas = []
+    ids = []
+    id = 10000
 
-        for shoppers in ["men", "women"]:
-            instruction = "Yor are a creative catalog writer."
-            for product in ["hats", "shirts", "pants", "shoes"]:
-                prompt = f"Invent a catlog description (about 200 words) and price for {shoppers}'s {product}. Pure text format, no markdown."
-                documents.append(llm.invoke(instruction, prompt))
-                metadatas.append({"section": f"{shoppers}'s {product}"})
-                ids.append(f"id-{id}")
-                id += 1
+    for shoppers in ["men", "women"]:
+        instruction = "Yor are a creative catalog writer."
+        for product in ["hats", "shirts", "pants", "shoes"]:
+            prompt = f"Invent a catlog description (about 200 words) and price for {shoppers}'s {product}. Pure text format, no markdown."
+            documents.append(llm.invoke(instruction, prompt))
+            metadatas.append({"section": f"{shoppers}'s {product}"})
+            ids.append(f"id-{id}")
+            id += 1
 
-        collection().add(documents=documents, metadatas=metadatas, ids=ids)
+    def create(name, documents, metadatas, ids):
+        collection(name).add(documents=documents, metadatas=metadatas, ids=ids)
