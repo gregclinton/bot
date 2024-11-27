0864a2ad1d405ed23c8ae1b9182d91e577abaa7b
Greg Clinton <greg.clinton@gmail.com>
2024-11-26 16:20:58 -0800

    latest

--- tools.py
+++ tools.py
@@ -3,14 +3,22 @@ import catalog
 import messages
 from messages import Message
 
+counter = 0
 def call_tool(tool, text):
     if tool == "Catalog":
         context = catalog.query(text)
-        return llm.invoke("Complete this.", f"Context: {context}\nQuestion: {text}\nAnswer: ")
+        return llm.invoke("Based on the context, provide the answer to the question.", f"Context: {context}\nQuestion: {text}\nAnswer: ")
 
     return "???"
 
 def invoke(tool, msgs):
+    global counter
+    if counter > 0 or msgs[0].sender != "Sales":
+        return ""
+        
+    counter += 1
+
+
     return messages.to_string([Message(tool, msgs[0].sender, "Sorry, it is not in our catalog.")])
     # First determine which tool msgs have not been answered.
     # Then for these, print out completion string consisting of tool answers with To: msg.sender From: tool and body answer
