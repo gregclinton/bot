1a7a7b6809bb0dd8af2f4d673c97e3a95ea16e21
Greg Clinton <greg.clinton@gmail.com>
2024-11-26 17:45:20 -0800

    latest

--- tools.py
+++ tools.py
@@ -26,8 +26,5 @@ def invoke(tool, msgs):
     for msg in unanswered:
         answers.append(Message(tool, msg.sender, call_tool(tool, msg.body)))
 
-    print(messages.to_string(answers) if answers else "")
-    exit()
-
     return messages.to_string(answers) if answers else ""
 
