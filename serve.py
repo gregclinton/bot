f3205576a19cc987fd64114b6a0da05bc2e34520
Greg Clinton <greg.clinton@gmail.com>
2024-11-27 09:53:31 -0800

    latest

--- serve.py
+++ serve.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, Request
-import company
+from company import invoke
 
 app = FastAPI()
 
-    @app.post('/company/{company}/messages/{caller}')
-async def post_message(req: Request, company:str, caller: str):
-    return { "content": company.invoke(company, caller, (await req.json())['prompt']) }
+@app.post('/company/{company}/messages/{caller}')
+async def post_message(req: Request, company: str, caller: str):
+    return { "content": invoke(company, caller, (await req.json())['prompt']) }
