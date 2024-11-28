b750ac3a25e12811dd0f6d3c3023eb514026cf45
Greg Clinton <greg.clinton@gmail.com>
2024-11-28 08:57:11 -0800

    latest

--- switchboard.py
+++ switchboard.py
@@ -1,10 +1,14 @@
 import llm
 import company
 import json
+import os
 
-input_instruction = """
+companies = ','.join(next(os.walk("ar"))[1])
+
+input_instruction = f"""
 Glean from the prompt a company and output JSON object
-with company field set to one of Sephora, Cox or Wendy's.
+
+Here is the list of available companies: {companies}
 
 Output raw JSON string without markdown.
 """
