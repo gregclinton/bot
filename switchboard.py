cbfb1251ba45458ddabb0cedbdf2d391a5415163
Greg Clinton <greg.clinton@gmail.com>
2024-11-29 15:51:26 -0800

    latest

--- switchboard.py
+++ switchboard.py
@@ -13,6 +13,6 @@ Here is the list of available companies: {companies}
 Output raw JSON string without markdown.
 """
 
-def invoke(company, query):
+def invoke(_company_, query):
     company.next = json.loads(llm.invoke(input_instruction, query))["company"]
     return f"Success. Switched to {company.next}"
