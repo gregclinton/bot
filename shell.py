e6d771b8e3bbad4ed7019c790e634a7f0f7c1c46
Greg Clinton <greg.clinton@gmail.com>
2024-11-29 11:34:11 -0800

    latest

--- shell.py
+++ shell.py
@@ -3,14 +3,10 @@ import llm
 
 input_instruction = """
 Give a prompt you will generate a shell command to access the user's computer.
-Path names are given relative to the current working directory.
 Output the raw command -- no markdown.
 """
 
 def invoke(query):
     command = llm.invoke(input_instruction, query)
-    print(query)
 
-    output = subprocess.run(command, shell=True, capture_output=True, text=True)
-    print(output)
-    return output
+    return subprocess.run(command, shell=True, capture_output=True, text=True).stdout
