07fdcc403e5ef5986656a5a9bb05c28e9ef9fc8e
Greg Clinton <greg.clinton@gmail.com>
2024-11-30 14:19:50 -0800

    latest

--- answers.py
+++ answers.py
@@ -3,15 +3,15 @@ import json
 import chroma
 
 input_instruction = f"""
-You are going to help us to create a ChromaDb vectorstore for providing information to the public.
-Glean from the prompt the company or entity in question, and generate 20 questions about that entity.
+You are going to help us to create a ChromaDb vectorstore for providing information to the public about {company}.
+Generate 20 questions about {company}.
 For each question, think of a unique one-word filename for that question.
-Output a JSON list of objects with filename key and question key.
-Output raw JSON string without markdown.
+Output a raw JSON list of objects with filename key and question key -- no markdown.
 """
 
 def invoke(company, query):
-    items = json.loads(llm.invoke(input_instruction, f"Questions about {company}"))
+    input_instruction = input_instruction.replace("{company}", company)
+    items = json.loads(llm.invoke(input_instruction, f"Generate the JSON."))
 
     for item in items:
         instruction = "Give a 1000-word answer to the question. Give the answer in raw text. No headers. No markdown."
