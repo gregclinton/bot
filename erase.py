4adeb4af82f6ed846f6adaf823b7909d8f45c10a
Greg Clinton <greg.clinton@gmail.com>
2024-12-02 13:18:18 -0800

    latest

--- erase.py
+++ erase.py
@@ -2,4 +2,4 @@ import messages
 
 def invoke(company, agent, caller, command):
     messages.delete_caller(company, caller)
-    return f"{caller} history has been deleted."
+    return f"{caller} thread has been deleted."
