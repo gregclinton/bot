4c82bd11e987bb59a138cdff719cd6e679069a7d
Greg Clinton <greg.clinton@gmail.com>
2024-12-02 08:00:31 -0800

    latest

--- chat.js
+++ chat.js
@@ -1,37 +1,37 @@
 const caller = 'account-375491'
 
 const chat = {
+    post: data => {
+        const name = data.company ? data.company : 'me';
+        const title = document.createElement('span');
+
+        title.innerHTML = name
+        title.classList.add('name');
+
+        const top = document.createElement('div');
+
+        top.append(title);
+
+        const bottom = document.createElement('div');
+
+        bottom.innerHTML = data.content;
+
+        const post = document.createElement('div');
+        post.append(top, bottom);
+        post.classList.add('post');
+        document.getElementById('chat').appendChild(post);
+
+        Prism.highlightAll();
+        MathJax.typesetPromise();
+
+        document.title = data.company;
+        post.scrollIntoView({ behavior: 'smooth' });
+    },
+
     prompt: async prompt => {
         chat.waiting = true;
 
-        function post(data) {
-            const name = data.company ? data.company : 'me';
-            const title = document.createElement('span');
-
-            title.innerHTML = name
-            title.classList.add('name');
-
-            const top = document.createElement('div');
-
-            top.append(title);
-
-            const bottom = document.createElement('div');
-
-            bottom.innerHTML = data.content;
-
-            const post = document.createElement('div');
-            post.append(top, bottom);
-            post.classList.add('post');
-            document.getElementById('chat').appendChild(post);
-
-            Prism.highlightAll();
-            MathJax.typesetPromise();
-
-            document.title = data.company;
-            post.scrollIntoView({ behavior: 'smooth' });
-        }
-
-        post({content: prompt});
+        chat.post({content: prompt});
 
         await fetch('/company/messages/' + caller, {
             method: 'POST',
@@ -44,8 +44,10 @@ const chat = {
             data.content = data.content.replace(/\\/g, '\\\\');  // so markdown won't trample LaTex
             data.content = marked.parse(data.content)
 
-            post(data);
+            chat.post(data);
             chat.waiting = false;
         });
     },
 };
+
+chat.post({company: "The Mall", content: "Welcome to The Mall."})
