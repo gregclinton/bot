cfe5ddb1271f1c6d15f62b2a7786e4c750bfbdc1
Greg Clinton <greg.clinton@gmail.com>
2024-12-03 14:28:29 -0800

    latest

--- plot.py
+++ plot.py
@@ -1,15 +1,30 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+code = """
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 2 * np.pi, 100)
+y = np.sin(x)
+
+plt.plot(x, y)
+plt.title('Sine Curve')
+plt.xlabel('x')
+plt.ylabel('sin(x)')
+
+plt.close()
+"""
+footer = """
+from io import StringIO
+buffer = StringIO()
+plt.savefig(buffer, format='svg')
+buffer.seek(0)
+svg_content = buffer.getvalue()
+buffer.close()
+"""
+
 def invoke(company, department, caller, command):
-    x = np.linspace(0, 2 * np.pi, 100)
-    y = np.sin(x)
-
-    plt.plot(x, y)
-    plt.title('Sine Curve')
-    plt.xlabel('x')
-    plt.ylabel('sin(x)')
-
-    plt.close()
-    return "plot"
+    exec(code + footer)
+    return svg_content
 
