8ad6a7770be75836489056fe833f0a76494ee1d9
Greg Clinton <greg.clinton@gmail.com>
2024-12-04 14:57:27 -0800

    latest

--- tool.py
+++ tool.py
@@ -1,15 +1,9 @@
 import chroma
 import shell
-import switchboard
-import answers
-import erase
 import plot
 
 bench = {
     "Database": chroma.invoke,
     "Shell": shell.invoke,
-    "Switchboard": switchboard.invoke,
-    "Answers": answers.invoke,
-    "Erase": erase.invoke,
     "Plot": plot.invoke,
 }
