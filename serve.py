f79875bba82b1f5a0c77a03cd4613493e4d2932d
Greg Clinton <greg.clinton@gmail.com>
2024-12-04 14:50:53 -0800

    latest

--- serve.py
+++ serve.py
@@ -3,40 +3,47 @@ import llm
 from tool import bench
 from time import sleep
 
-app = FastAPI()
 entities = {}
 max_llm_invokes = 10
 
 def invoke(entity, thread, prompt):
     instructions = open(f"ar/{entity}").read()
+    make_message = lambda role, content: { "role": role, "content": content }
+    system = lambda content: make_message("system", content)
+    user = lambda content: make_message("user", content)
+    assistant = lambda content: make_message("assistant", content)
+
     messages = entities.setdefault(entity, {}).setdefault(thread, [])
-    messages.append({"role": "user", "content": prompt})
-    response = llm.invoke([{"role": "system", "content": instructions}] + messages)
+    messages.append(user(prompt))
+    response = llm.invoke([system(instructions)] + messages)
 
     if "entity" in response:
         return response
 
-    while "content" not in response and llm.counter < max_llm_invokes:
-        if "path" in response:
+    while "content" not in response:
+        sleep(0.2) # in case this loop runs away
+        if llm.counter > max_llm_invokes:
+            response = { "content": "Could you please rephrase that?" }
+        elif "path" in response:
             response = invoke(response["path"], "111222", response["prompt"])
         elif "tool" in response:
-            sleep(0.2)
             tool = response["tool"]
             prompt = response["prompt"]
-            messages.append({"role": "assistant", "content": f"tool: {tool}, prompt: {prompt}"})
+            messages.append(assistant(f"tool: {tool}, prompt: {prompt}"))
             if tool in bench:
                 try:
                     output = bench[tool]("", "", "", prompt)
-                    messages.append({"role": "assistant", "content": f"tool response: {output}"})
-                    response = llm.invoke([{"role": "system", "content": instructions}] + messages)
+                    messages.append(assistant(f"tool response: {output}"))
+                    response = llm.invoke([system(instructions)] + messages)
                 except Exception as e:
                     response = { "content": str(e) }
             else:
                 response = { "content": "I made a mistake with a tool called " + response["tool"] + ". Apparently, there is no such tool." }
 
-    messages.append({"role": "assistant", "content": response["content"]})
+    messages.append(assistant(response["content"]))
     return response
 
+app = FastAPI()
 
 @app.post('/mall/{entity}/messages/{thread}')
 async def post_message(req: Request, entity: str, thread: str):
