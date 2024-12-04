93238268561e1aa6271d7d75f3c2e6e242d125d0
Greg Clinton <greg.clinton@gmail.com>
2024-12-04 11:08:22 -0800

    latest

--- serve.py
+++ serve.py
@@ -12,15 +12,18 @@ def invoke(entity, thread, prompt):
     messages = entities.setdefault(entity, {}).setdefault(thread, [])
     messages.append({"role": "user", "content": prompt})
     response = llm.invoke([{"role": "system", "content": instructions}] + messages)
+
     if "tool" in response:
         if response["tool"] in tool.bench:
             try:
-                completion = { "content": tool.bench[msg.to_]("", "", "", completion["prompt"]) }
+                response = { "content": tool.bench[response["tool"]]("", "", "", response["prompt"]) }
                 del response["tool"]
             except Exception as e:
                 response = { "content": str(e) }
-    else:
-        messages.append({"role": "assistant", "content": response["content"]})
+        else:
+            response = { "content": "I made a mistake with a tool called " + response["tool"] + ". Apparently, there is no such tool." }
+
+    messages.append({"role": "assistant", "content": response["content"]})
     return response
 
 @app.post('/mall/{entity}/messages/{thread}')
