bb30cb1bf0b783b6ba280158fec452ae8098c41d
Greg Clinton <greg.clinton@gmail.com>
2024-12-05 09:12:06 -0800

    latest

--- tools/chroma.py
+++ tools/chroma.py
@@ -4,11 +4,11 @@ import llm
 import os
 import json
 
-def client(path):
-    return chromadb.PersistentClient(path=f"chroma/{path}")
+def client():
+    return chromadb.PersistentClient(path=f"chroma")
 
-def collection(path, name):
-    return client(path).get_or_create_collection(
+def collection(name):
+    return client().get_or_create_collection(
         name=name,
         embedding_function=embedding_functions.OpenAIEmbeddingFunction(
             api_key=os.environ["OPENAI_API_KEY"],
@@ -16,50 +16,51 @@ def collection(path, name):
         )
     )
 
-def delete_collection(path, name):
-    client(path).delete_collection(name)
+def delete_collection(name):
+    client().delete_collection(name)
 
-def invoke(args):
+def invoke(search):
     input_instruction = """
 Currently we have the following databases: {collections}
 From the user prompt generate the most appropriate database and search to use.
 Output JSON object with database key, a string, and search key, also a string.
 Output the raw JSON without markdown.
 """
-    collections = ", ".join(map(lambda collection:  collection.name, client(args["path"]).list_collections()))
+    collections = ", ".join(map(lambda collection:  collection.name, client().list_collections()))
 
     if collections:
         o = json.loads(llm.invoke([
             { "role": "system", "content": input_instruction.replace("{collections}", collections)},
-            { "role": "user", "content": args["search"]}
+            { "role": "user", "content": search}
         ])["content"])
         collection_name = o["database"]
-        search = o["search"]
-        results = " ".join(collection(args["path"], collection_name).query(query_texts=[search], n_results=4)["documents"][0])
+        results = " ".join(collection(collection_name).query(query_texts=[search], n_results=4)["documents"][0])
         return f"Our search of the {collection_name} database yielded the following result: \n{results}"
     else:
         return "As of yet, we have no databases."
 
-def create_collection_from_huge_text(path, name, text):
+def create_collection_from_huge_text(name, text):
     chunk_size = 2000
     overlap = 100
     chunking = range(0, len(text), chunk_size - overlap)
     documents = [text[i : i + chunk_size] for i in chunking]
     metadatas = [{"id": i} for i in chunking]
     ids = [str(i) for i in chunking]
-    collection(path, name).add(documents=documents, metadatas=metadatas, ids=ids)
+    collection(name).add(documents=documents, metadatas=metadatas, ids=ids)
 
-def create_answers_collection(path):
+def create_collection_from_files_in_answer_folder(name):
     documents = []
     metadatas = []
     ids = []
 
     i = 100000
 
-    for file in filter(os.path.isfile, map(lambda f: os.path.join(f"answers/{path}/", f), os.listdir(f"answers/{path}/"))):
+    for file in filter(os.path.isfile, map(lambda f: os.path.join(f"answers/{name}/", f), os.listdir(f"answers/{name}/"))):
          documents.append(open(file, "r").read())
          metadatas.append(os.path.basename(file))
          ids.append(str(i))
          i += 1
 
-    collection(path, "answers").add(documents=documents, metadatas=metadatas, ids=ids)
+    collection(name).add(documents=documents, metadatas=metadatas, ids=ids)
+
+create_collection_from_files_in_answer_folder("Medicare")
