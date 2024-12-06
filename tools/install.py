a0062daa8bcda6ac57a9ebf7012fc1a1387c0663
Greg Clinton <greg.clinton@gmail.com>
2024-12-06 09:01:08 -0800

    latest

--- tools/install.py
+++ tools/install.py
@@ -1,4 +1,13 @@
+from importlib import import_module
+
 def invoke(tools, thread):
     tools = tools.split("\n")[0].strip()
-    thread["installed"].update(tools.split(","))
-    return f"As you requested, I installed {tools}. You can now use it to answer my above request." 
+    output = ""
+    for tool in tools.split(","):
+        tool =  tool.strip()
+        if import_module(tool):
+            thread["installed"].add(tool)
+            output += f"The {tool} tool was successfully installed. You can now use it to answer my above request.\n"
+        else:
+            output += f"The {tool} tool was not found.\n"
+    return output
