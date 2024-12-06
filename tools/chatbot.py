e983bc4219187ef79cc946f5322408bffb76170f
Greg Clinton <greg.clinton@gmail.com>
2024-12-06 15:39:30 -0800

    latest

--- tools/chatbot.py
+++ tools/chatbot.py
@@ -3,11 +3,11 @@ import requests
 def invoke(text, thread):
     url, prompt = text.split("\n")
     headers = { "Content-Type": "text/plain" }
-    post = lambda path, data = "": requests.post(f"{url}/{path}", data = data, headers = headers).body()
+    post = lambda path, data = "": requests.post(f"{url}/{path}", data = data, headers = headers).text
 
     url = url.strip()
     if url not in thread["bots"]:
-        thread["bots"][url] = id = post("threads")["id"]
+        thread["bots"][url] = id = post("threads")
     else:
         id = thread["bots"][url]
 
