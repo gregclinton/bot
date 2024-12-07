8bc3d55cd066de3d1fb4eeffae240137a664a528
Greg Clinton <greg.clinton@gmail.com>
2024-12-07 06:48:59 -0800

    latest

--- tools/install.py
+++ tools/install.py
@@ -5,7 +5,7 @@ def invoke(tools, thread):
     tools = tools.split("\n")[0].strip()
     output = ""
     for tool in tools.split(","):
-        tool =  tool.strip()
+        tool =  tool.strip().lower()
         if os.path.isfile(f"how/{tool}"):
             installed = thread["installed"]
             installed.append(tool) if tool not in installed else None
