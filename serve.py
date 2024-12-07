204f81c18ec6cfc0f763784ed45a0dc0f27fc332
Greg Clinton <greg.clinton@gmail.com>
2024-12-07 09:35:56 -0800

    latest

--- serve.py
+++ serve.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI, Request
 from fastapi.responses import PlainTextResponse
 import logging
 import chat
 
 app = FastAPI(default_response_class=PlainTextResponse)
 
-if False:
+if True:
     logging.getLogger("uvicorn.access").disabled = True
     logging.getLogger("uvicorn.error").disabled = True
     logging.getLogger("fastapi").disabled = True
@@ -15,11 +15,10 @@ print("I'm up.", flush=True)
 threads = {}
 
 def clear(id):
-    if id in threads:
-        for url, id in enumerate(threads[id]["bots"]):
-            requests.delete(f'{url}/threads/{id}', headers = { 'Content-Type': 'text/plain' })
+    for url, bot_id in threads.get(id, {}).get("bots", {}).items():
+        requests.delete(f'{url}/threads/{bot_id}', headers={ 'Content-Type': 'text/plain' })
 
-    threads[id] = { "messages": [], "installed" : ["brevity", "install"], "bots": {} }
+    threads.setdefault(id, {"messages": [], "installed": ["brevity", "install"], "bots": {}})
     return id
 
 @app.post('/threads/{id}/messages')
@@ -27,24 +26,24 @@ async def post_message(req: Request, id: str):
     return chat.run(threads[id], (await req.body()).decode("utf-8"))
 
 @app.delete('/threads/{id}')
-async def delete_thread(req: Request, id: str):
+async def delete_thread(id: str):
     threads.pop(id, None)
     return "success"
 
 @app.delete('/threads/{id}/messages')
-async def delete_messages(req: Request, id: str):
+async def delete_messages(id: str):
     clear(id)
     return "success"
 
 @app.delete('/threads/{id}/messages/last')
-async def delete_last_message(req: Request, id: str):
+async def delete_last_message(id: str):
     threads[id]["messages"].pop()
     return "success"
 
 id = 111111
 
 @app.post('/threads')
-async def post_thread(req: Request):
+async def post_thread():
     global id
     id += 1
     return clear(str(id))
