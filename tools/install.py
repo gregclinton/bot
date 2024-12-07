54d29c34caecf24215bfaf1694fee967c1fa7ff7
Greg Clinton <greg.clinton@gmail.com>
2024-12-06 17:42:14 -0800

    latest

--- tools/install.py
+++ tools/install.py
@@ -5,11 +5,11 @@ def invoke(tools, thread):
     output = ""
     for tool in tools.split(","):
         tool =  tool.strip()
-        output += f"The {tool} tool was "
         if import_module(f"tools.{tool}"):
             installed = thread["installed"]
             installed.append(tool) if tool not in installed else None
-            output += "successfully installed.\n"
+            result = "successfully installed"
         else:
-            output += "not found.\n"
+            result = "not found"
+        output += f"The {tool} tool was {result}.\n"
     return output
