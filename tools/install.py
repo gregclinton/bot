e92a85e75fddac54bfff6f4bfef5d73ee8d93030
Greg Clinton <greg.clinton@gmail.com>
2024-12-07 06:47:17 -0800

    latest

--- tools/install.py
+++ tools/install.py
@@ -1,11 +1,12 @@
 from importlib import import_module
+import os
 
 def invoke(tools, thread):
     tools = tools.split("\n")[0].strip()
     output = ""
     for tool in tools.split(","):
         tool =  tool.strip()
-        if import_module(f"tools.{tool}"):
+        if os.path.isfile(f"how/{tool}"):
             installed = thread["installed"]
             installed.append(tool) if tool not in installed else None
             result = "successfully installed"
