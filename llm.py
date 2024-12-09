32143f049c732d3b6153df00425c6ac522511df2
Greg Clinton <greg.clinton@gmail.com>
2024-12-09 11:17:59 -0800

    latest

--- llm.py
+++ llm.py
@@ -12,33 +12,41 @@ for tool in os.listdir("tools"):
     import_module(f"tools.{tool}").create(tools)
 
 def invoke(messages, thread={}):
-    message = requests.post(
-        "https://api.openai.com/v1/chat/completions",
-        headers = {
-            'Authorization': 'Bearer ' + os.environ['OPENAI_API_KEY'],
-            'Content-Type': 'application/json',
-        },
-        json = {
-            "model": thread.get("model", "gpt-4o"),
-            "temperature": thread.get("temperature", 0),
-            "messages": messages,
-            "tools": tools,
-            "tool_choice": "auto"
-        }).json()["choices"][0]["message"]
+    count = 0
+    max_count = 10
+    content = None
 
-    for call in message.get("tool_calls", []):
-        try:
-            fn = call["function"]
+    while not content and count < 10:
+        count += 1
+        message = requests.post(
+            "https://api.openai.com/v1/chat/completions",
+            headers = {
+                'Authorization': 'Bearer ' + os.environ['OPENAI_API_KEY'],
+                'Content-Type': 'application/json',
+            },
+            json = {
+                "model": thread.get("model", "gpt-4o"),
+                "temperature": thread.get("temperature", 0),
+                "messages": messages,
+                "tools": tools,
+                "tool_choice": "auto"
+            }).json()["choices"][0]["message"]
 
-            messages.append({
-                "role":"tool",
-                "tool_call_id": call["id"],
-                "name": fn["name"],
-                "content": import_module("tools." + fn["name"]).run(json.loads(fn["arguments"])["text"], thread)
-            })
-        except Exception as e:
-            return str(e)
+        content = message.content
+
+        for call in message.get("tool_calls", []):
+            try:
+                fn = call["function"]
+
+                messages.append({
+                    "role":"tool",
+                    "tool_call_id": call["id"],
+                    "name": fn["name"],
+                    "content": import_module("tools." + fn["name"]).run(json.loads(fn["arguments"])["text"], thread)
+                })
+            except Exception as e:
+                return str(e)
 
         return "tool call"
 
-    return message["content"]
+    return content or "Could you rephrase that, please?"
