ba79de165095eac3f09262f06ef26122ea9a521b
Greg Clinton <greg.clinton@gmail.com>
2024-12-09 06:52:09 -0800

    latest

--- serve.py
+++ serve.py
@@ -10,7 +10,7 @@ def clear(id):
     for url, bot_id in threads.get(id, {}).get("bots", {}).items():
         requests.delete(f'{url}/threads/{bot_id}', headers={ 'Content-Type': 'text/plain' })
 
-    threads[id] = { "messages": [], "docs": ["brevity", "tools", "docs"], "bots": {}, "runs": [] }
+    threads[id] = { "messages": [], "docs": ["brevity", "tools", "docs", "shell"], "bots": {}, "runs": [] }
     return id
 
 @app.post('/threads/{id}/messages')
