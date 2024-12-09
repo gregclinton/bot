5acebf0e79c920b852e0e980a966600dfcd0396b
Greg Clinton <greg.clinton@gmail.com>
2024-12-09 11:55:09 -0800

    latest

--- tools/model.py
+++ tools/model.py
@@ -3,7 +3,7 @@ import os
 def descriptions():
     return (
         "Specify llm model and temperature.",
-        "model,temperature"
+        "model,temperature: Model can be gpt-4o or gpt-4o-mini. Temperature is from 0 to 100."
     )
 def run(text, thread):
     model, temperature = (s.strip() for s in (text.split(",") + ["0"])[:2])
