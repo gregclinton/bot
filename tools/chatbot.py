8e95b0ba68a1781204d5486016634d40203b1680
Greg Clinton <greg.clinton@gmail.com>
2024-12-12 09:04:29 -0800

    latest

--- tools/chatbot.py
+++ tools/chatbot.py
@@ -1,8 +1,10 @@
 import requests
 
+tool = __name__[6:] # strip "tools."
+
 def restart(thread):
-    me = thread["tools"].get("chatbot", {})
-    me["bots"] = bots = me.get("bots", {})
+    data = thread["tools"].get(tool, {})
+    data["bots"] = bots = data.get("bots", {})
     for url, id in bots.items():
         requests.delete(f'{url}/threads/{id}', headers={ 'Content-Type': 'text/plain' })
     bots.clear()
@@ -27,13 +29,13 @@ def meta():
 
 def run(args, thread):
     url = args["url"]
-    me = thread["tools"]["chatbot"]
+    bots = thread["tools"][tool]["bots"]
     headers = { "Content-Type": "text/plain" }
     post = lambda path, data = "": requests.post(f"{url}/{path}", data = data, headers = headers).text
 
-    if url not in me["bots"]:
-        me["bots"][url] = id = post("threads")
+    if url not in bots
+        bots[url] = id = post("threads")
     else:
-        id = me["bots"][url]
+        id = bots[url]
 
     return post(f"threads/{id}/messages", args["prompt"])
