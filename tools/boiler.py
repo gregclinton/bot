81a80eb60ddc159d3cb0bee2767c762311979362
Greg Clinton <greg.clinton@gmail.com>
2024-12-13 13:20:20 -0800

    latest

--- tools/boiler.py
+++ tools/boiler.py
@@ -3,4 +3,4 @@ from boilerpy3 import extractors
 def run(url: str, thread: dict):
     "Downloads page with the given url and returns text cleaned by boilerpy3."
 
-    return extractors.ArticleExtractor().get_content_from_url(url)
+    return extractors.KeepEverythingExtractor().get_content_from_url(url)
