de88cb1a611ac648dd20fe6513263da20866965f
Greg Clinton <greg.clinton@gmail.com>
2024-12-13 14:27:02 -0800

    latest

--- tools/recap.py
+++ tools/recap.py
@@ -4,7 +4,7 @@ def run(summary: str, thread: dict):
  figuring out a tricky REST API. It involves a lot of web searches and
  trial and error attempts. You only may have gleaned a couple of nuggets.
  Pass what you know so far in the summary parameter. The rest of the tool output
- will be purged to save me token money. And you will have captured the
+ will be purged to save me, the user, token money. And you will have captured the
  fruits of your labor and lightened the token load.
  """
     return summary
