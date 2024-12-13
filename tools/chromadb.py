311718ee3972ed302c64aa8b48ec8ad00ca29a2d
Greg Clinton <greg.clinton@gmail.com>
2024-12-13 06:41:09 -0800

    latest

--- tools/chromadb.py
+++ tools/chromadb.py
@@ -5,8 +5,14 @@ import logging
 
 def run(collection: str, search: str, thread: dict):
     "Searches the given chromadb collection using the given search string."
-    collection = chromadb.PersistentClient(path="chroma").get_or_create_collection(name=collection)
-    return "\n".join(collection.query(query_texts=search, n_results=3)["documents"][0])
+    client = chromadb.PersistentClient(path="chroma")
+    collections = [collection.name for collection in client.list_collections()]
+
+    if collection in collections:
+        collection = client.get_or_create_collection(name=collection)
+        return "\n".join(collection.query(query_texts=search, n_results=3)["documents"][0])
+    else:
+        return f"The collection \"{collection}\" was not found. Our collections include: " + ", ".join(collections)
 
 
 logging.getLogger('chromadb').setLevel(logging.ERROR)
@@ -23,4 +29,4 @@ each being a pizza choice with wonderful description and price
 at Giovanni's, a fictious Chicago pizzeria.
 """)
 
-# chromadb.PersistentClient(path="chroma").delete_collection("Medicare")
+# chromadb.PersistentClient(path="chroma").delete_collection("medicare")
