d66a2127b23de842b5f014aee796c8fcc4b97785
Greg Clinton <greg.clinton@gmail.com>
2024-12-13 06:44:13 -0800

    latest

--- tools/chatbot.py
+++ tools/chatbot.py
@@ -1,19 +1,19 @@
 import requests
 
 tool = __name__[6:] # strip "tools."
+headers = { "Content-Type": "text/plain" }
 
 def reset(thread):
     tools = thread["tools"]
     data = tools[tool] = tools.get(tool, {})
     data["bots"] = bots = data.get("bots", {})
     for url, id in bots.items():
-        requests.delete(f'{url}/threads/{id}', headers={ 'Content-Type': 'text/plain' })
+        requests.delete(f'{url}/threads/{id}', headers = headers)
     bots.clear()
 
 def run(url: str, prompt: str, thread: dict):
     "Talks with another chatbot at the given url and the given prompt."
     bots = thread["tools"][tool]["bots"]
-    headers = { "Content-Type": "text/plain" }
     post = lambda path, data = "": requests.post(f"{url}/{path}", data = data, headers = headers).text
 
     if url not in bots:
