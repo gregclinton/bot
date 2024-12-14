1604ca4161c99585cf541d92babf13472485a100
Greg Clinton <greg.clinton@gmail.com>
2024-12-13 16:21:28 -0800

    latest

--- tools/model.py
+++ tools/model.py
@@ -1,3 +1,7 @@
+def reset(thread):
+    thread["model"] = "gtp-4o"
+    thread["temperature"] = 0
+    
 def run(model: str, temperature: int, thread: dict):
     "Sets the llm model, gtp-4o or gpt-4o-mini, and temperature, 0 to 100."
     thread["model"] = model
