c6ecf20830c512efaa25013f829d4be413fcd061
Greg Clinton <greg.clinton@gmail.com>
2024-12-15 07:25:03 -0800

    latest

--- tools/search.py
+++ tools/search.py
@@ -15,6 +15,7 @@ def run(query: str, thread: dict):
     text = f"Mish-mash of google search results from today, {today}, extracted by boilerpy3: \n\n\n"
 
     extractor = extractors.KeepEverythingExtractor()
+    count = 0
 
     for item in requests.get(
         "https://customsearch.googleapis.com/customsearch/v1",
@@ -22,13 +23,18 @@ def run(query: str, thread: dict):
             "key": os.environ["CUSTOM_SEARCH_API_KEY"],
             "cx": os.environ["CUSTOM_SEARCH_CX"],
             "q": query,
-            "num": 3
+            "num": 5
         }).json()["items"]:
 
-        res = requests.get(item["link"], headers = {'User-Agent': 'Chrome/50.0.2661.102'})
+        try:
+            res = requests.get(item["link"], headers = {'User-Agent': 'Chrome/50.0.2661.102'}, timeout = 5)
+            if res.ok:
+                text += extractor.get_content(res.text).replace("\n", " ")
+                count += 1
+        except:
+            pass
 
-        if res.ok:
-            text += extractor.get_content(res.text).replace("\n", " ")
+        if count == 3:
+            break
 
     return llm.mini(text + f"\n\n\nQuery:\n{query}\nAnswer:\n")
-
