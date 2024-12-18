c7b04df8520a84d51dd45101f2e90995b42c3693
Greg Clinton <greg.clinton@gmail.com>
2024-12-18 06:53:24 -0800

    latest

--- tools/shell.py
+++ tools/shell.py
@@ -3,4 +3,4 @@ import subprocess
 def run(command: str, thread: dict):
     "Runs the given command, like ls, cat, sed, echo, curl, python3, etc. in a Linux shell."
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
-    return result.stdout or result.error
+    return result.stdout or result.stderr
