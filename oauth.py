36078412550c13df5a086a66f0efe1c099413e4c
Greg Clinton <greg.clinton@gmail.com>
2024-12-22 13:02:10 -0800

    latest

--- oauth.py
+++ oauth.py
@@ -1,38 +1,48 @@
-import os, base64, hashlib, secrets, httpx
+import os, httpx, base64, hashlib, secrets
 
 # https://fhir.epic.com/Developer/Apps
 # https://open.epic.com/MyApps/endpoints
+# https://github.com/settings/applications/2819909
 
-base_url = {
-    # "eric": "https://fhir.epic.com/interconnect-fhir-oauth" # sandbox
-    "eric": "https://fhir.kp.org/service/ptnt_care/EpicEdiFhirRoutingSvc/v2014/esb-envlbl/212", # production
-}
-
-base_redirect_uri = "https://192.168.1.13/oauth"
+def base_url(name):
+    return {
+        "epic": "https://fhir.epic.com/interconnect-fhir-oauth/oauth2",
+        "github": "https://github.com/login/oauth"
+    }
 
 def redirect(name):
-    os.environ[f"{name.upper()}_CODE_VERIFIER"] = code_verifier = secrets.token_urlsafe(64)
-    code_challenge = base64.urlsafe_b64encode(hashlib.sha256(code_verifier.encode()).digest()).rstrip(b"=").decode()
-
-    return f"{base_url[name]}/oauth2/authorize?" + "&".join(f"{k}={v}" for k, v in {
+    params = {
         "response_type": "code",
         "client_id": os.environ[f"{name.upper()}_CLIENT_ID"],
-        "redirect_uri": f"{base_redirect_uri}/{name}",
-        "scope": "patient/*.read",
-        "code_challenge": code_challenge,
-        "code_challenge_method": "S256",
-        "state": "random_state_string",
-        "iss": f"{base_url[name]}/api/FHIR/R4"
-    }.items())
+        "redirect_uri": f"https://192.168.1.13/oauth/{name}",
+        "state": "nil",
+    }
+
+    secret = os.environ[f"{name.upper()}_CLIENT_SECRET"]
+
+    if secret:
+        params["client_secret"] = secret
+    else:
+        os.environ[f"{name.upper()}_CODE_VERIFIER"] = code_verifier = secrets.token_urlsafe(64)
+        code_challenge = base64.urlsafe_b64encode(hashlib.sha256(code_verifier.encode()).digest()).rstrip(b"=").decode()
+
+        params.update({
+            "scope": "patient/*.read",
+            "code_challenge": code_challenge,
+            "code_challenge_method": "S256",
+            "iss": base_url(name)
+        })
+
+    return f"{base_url(name)}/authorize?" + "&".join(f"{k}={v}" for k, v in params.items())
 
 async def callback(code, name):
     async with httpx.AsyncClient() as client:
         res = await client.post(
-            f"{base_url[name]}/oauth2/token",
+            f"{base_url(name)}/oauth2/token",
             data = {
                 "grant_type": "authorization_code",
                 "code": code,
-                "redirect_uri": f"{base_redirect_uri}/{name}",
+                "redirect_uri": f"https://192.168.1.13/oauth/{name}",
                 "client_id": os.environ[f"{name.upper()}_CLIENT_ID"],
                 "code_verifier": os.environ[f"{name.upper()}_CODE_VERIFIER"]
             }
