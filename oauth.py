0be5421819a9004db0ee13e81757254f06d12b83
Greg Clinton <greg.clinton@gmail.com>
2024-12-22 10:51:11 -0800

    latest

--- oauth.py
+++ oauth.py
@@ -8,6 +8,8 @@ base_url = {
     "eric": "https://fhir.kp.org/service/ptnt_care/EpicEdiFhirRoutingSvc/v2014/esb-envlbl/212", # production
 }
 
+base_redirect_uri = "https://192.168.1.13/oauth"
+
 def redirect(name):
     os.environ[f"{name.upper()}_CODE_VERIFIER"] = code_verifier = secrets.token_urlsafe(64)
     code_challenge = base64.urlsafe_b64encode(hashlib.sha256(code_verifier.encode()).digest()).rstrip(b"=").decode()
@@ -15,7 +17,7 @@ def redirect(name):
     return f"{base_url[name]}/oauth2/authorize?" + "&".join(f"{k}={v}" for k, v in {
         "response_type": "code",
         "client_id": os.environ[f"{name.upper()}_CLIENT_ID"],
-        "redirect_uri": f"https://192.168.1.13/oauth/{name}",
+        "redirect_uri": f"{base_redirect_uri}/{name}",
         "scope": "patient/*.read",
         "code_challenge": code_challenge,
         "code_challenge_method": "S256",
@@ -30,7 +32,7 @@ async def callback(code, name):
             data = {
                 "grant_type": "authorization_code",
                 "code": code,
-                "redirect_uri": f"https://192.168.1.13/oauth/{name}",
+                "redirect_uri": f"{base_redirect_uri}/{name}",
                 "client_id": os.environ[f"{name.upper()}_CLIENT_ID"],
                 "code_verifier": os.environ[f"{name.upper()}_CODE_VERIFIER"]
             }
