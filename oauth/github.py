41b33d13e48295fcc50f68220407955e914e5ef8
Greg Clinton <greg.clinton@gmail.com>
2024-12-23 14:53:42 -0800

    latest

--- oauth/github.py
+++ oauth/github.py
@@ -7,27 +7,24 @@ import httpx
 
 def run(app):
     base_url = "https://github.com/login/oauth"
-    redirect_uri = "https://192.168.1.13/oauth/github"
 
     @app.get("/oauth/github/login")
     async def login(name: str):
-        return RedirectResponse(f"{base_url}/oauth2/authorize?" + "&".join(f"{k}={v}" for k, v in {
-            "response_type": "code",
+        return RedirectResponse(f"{base_url}/authorize?" + "&".join(f"{k}={v}" for k, v in {
             "client_id": os.environ["GITHUB_CLIENT_ID"],
-            "client_secret": os.environ["GITHUB_CLIENT_SECRET"],
-            "redirect_uri": redirect_uri,
+            "scope": "read:user",
+            "redirect_uri": "https://192.168.1.13/oauth/github",
         }.items()))
 
     @app.get("/oauth/github")
     async def callback(req: Request):
         async with httpx.AsyncClient() as client:
-            res = await client.post(f"{base_url[provider]}/oauth2/token", data = {
-                "grant_type": "authorization_code",
+            res = await client.post(f"{base_url}/access_token", data = {
                 "code": req.query_params.get("code"),
-                "redirect_uri": redirect_uri,
                 "client_id": os.environ["GITHUB_CLIENT_ID"],
+                "client_secret": os.environ["GITHUB_CLIENT_SECRET"],
             })
             res.raise_for_status()
-            with open(f"auth.json.epic.{provider}", "w") as f:
+            with open("auth.json.github", "w") as f:
                 f.write(res.text)
             return "You are now logged in. You can close this tab."
