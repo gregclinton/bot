d070c334518f4c2e285e01a088a1ab9d7a9ae34d
Greg Clinton <greg.clinton@gmail.com>
2024-12-27 08:13:05 -0800

    latest

--- tools/agent.py
+++ tools/agent.py
@@ -1,22 +1,22 @@
 import chat
 
-name = __name__[6:] # strip "tools." 
+modeule_name = __name__[6:] # strip "tools." 
 
 def reset(thread):
     thread["tools"] = tools = thread.get("tools", {})
-    tools[name] = data = tools.get(name, {})
+    tools[modeule_name] = data = tools.get(modeule_name, {})
     data["agents"] = agents = data.get("agents", {})
     agents.clear()
 
-def run(name: str, tools: str, docs: str, prompt: str, thread: dict):
+def run(agent_name: str, tools: str, docs: str, prompt: str, thread: dict):
     """
     Creates an agent, or uses an existing agent with a comma-delimeted set of tools
     and a comma-delimeted set of docs. The agent will respond to the prompt.
     When finished with the agent, sign off by saying 'bye'.
     """
-    agents = thread["tools"][name]["agents"]
+    agents = thread["tools"][modeule_name]["agents"]
 
-    if name not in agents:
-        agents[name] = chat.reset({ "use": { "docs": docs, "tools": tools } })
+    if agent_name not in agents:
+        agents[agent_name] = chat.reset({ "use": { "docs": docs, "tools": tools } })
 
-    return chat.run(prompt, agents[name]) 
+    return chat.run(prompt, agents[agent_name]) 
