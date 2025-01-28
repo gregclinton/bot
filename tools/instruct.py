ec3ad8bc9e1162673551d31b5a86dba5c1c30267
Greg Clinton <greg.clinton@gmail.com>
2025-01-28 10:05:30 -0800

    latest

--- tools/instruct.py
+++ tools/instruct.py
@@ -3,4 +3,4 @@ def run(instructions: str, thread: dict):
     Overwrites the system message with the given instructions.
     """
     thread["messages"][0]["content"] = instructions
-    return "ok"
+    return "success"
