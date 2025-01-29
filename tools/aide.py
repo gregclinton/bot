7d43c8c4e2f4ad6283c663b9274a4d674dd13bdb
Greg Clinton <greg.clinton@gmail.com>
2025-01-29 09:42:20 -0800

    latest

--- tools/aide.py
+++ tools/aide.py
@@ -14,6 +14,9 @@ def run(aide_name: str, prompt: str, thread: dict):
     This helps avoid context window bloat as each aide will have its own context window.
     Aides can have their own aides or a hierarchy of aides.
 
+    You can also think of aides as specialists.
+    Divvy up a knowledge base into specialized silos and assign an aide to each one.
+
     You yourself might be an aide and not know it. No way to know.
     """
     aides = thread["aides"]
