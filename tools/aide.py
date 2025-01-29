d5e23c1beb211a39d53ee75ab1c3afc81ce5fa16
Greg Clinton <greg.clinton@gmail.com>
2025-01-29 09:39:30 -0800

    latest

--- tools/aide.py
+++ tools/aide.py
@@ -8,7 +8,13 @@ def run(aide_name: str, prompt: str, thread: dict):
     Prompts an aide with the given prompt.
     If the aide does not already exist it will be created.
     When finished with the aide, sign off by saying 'bye'.
-    Use the instruct tool to keep track of aide names and their purposes.
+    Use the instruct tool to keep track of your aide names and their purposes.
+
+    With aides you can divvy up a large job into many smaller jobs.
+    This helps avoid context window bloat as each aide will have its own context window.
+    Aides can have their own aides or a hierarchy of aides.
+
+    You yourself might be an aide and not know it. No way to know.
     """
     aides = thread["aides"]
 
