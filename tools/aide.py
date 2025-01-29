c0734221f241153e94849b26af1032b196655834
Greg Clinton <greg.clinton@gmail.com>
2025-01-29 09:31:46 -0800

    latest

--- tools/aide.py
+++ tools/aide.py
@@ -3,16 +3,16 @@ import chat
 def reset(thread):
     thread["aides"] = {}
 
-def run(name: str, prompt: str, thread: dict):
+def run(aide_name: str, prompt: str, thread: dict):
     """
-    Prompts a aide with the given prompt.
+    Prompts an aide with the given prompt.
     If the aide does not already exist it will be created.
     When finished with the aide, sign off by saying 'bye'.
-    Use the instruct tool to keep track of aide names and their purpose.
+    Use the instruct tool to keep track of aide names and their purposes.
     """
     aides = thread["aides"]
 
-    if name not in aides:
-        aides[name] = chat.reset({})
+    if aide_name not in aides:
+        aides[aide_name] = chat.reset({})
 
-    return chat.run(prompt, aides[name]) 
+    return chat.run(prompt, aides[aide_name]) 
