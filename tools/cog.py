41ae96a92e8a1ba83f60d844e5109021045627a1
Greg Clinton <greg.clinton@gmail.com>
2025-02-05 09:56:36 -0800

    latest

--- tools/cog.py
+++ tools/cog.py
@@ -5,7 +5,7 @@ def reset(thread):
 
 def run(cog_name: str, prompt: str, thread: dict):
     """
-    Prompts an cog with the given prompt.
+    Prompts a cog with the given prompt.
     If the cog does not already exist it will be created.
     When finished with the cog, sign off by saying 'bye'.
     Use the instruct tool to keep track of your cog names and their purposes.
@@ -15,9 +15,9 @@ def run(cog_name: str, prompt: str, thread: dict):
     cogs can have their own cogs or a hierarchy of cogs.
 
     You can also think of cogs as specialists.
-    Divvy up a knowledge base into specialized silos and assign an cog to each one.
+    Divvy up a knowledge base into specialized silos and assign a cog to each one.
 
-    You yourself might be an cog and not know it. No way to know.
+    You yourself might be a cog and not know it. No way to know.
     """
     cogs = thread["cogs"]
 
