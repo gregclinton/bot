599cd28a4010b5ed7facc089bdffa243f8bdf3fc
Greg Clinton <greg.clinton@gmail.com>
2025-02-07 14:51:27 -0800

    latest

--- tools/shell.py
+++ tools/shell.py
@@ -3,7 +3,6 @@ import subprocess
 def run(command: str, thread: dict):
     """
     Runs the given command, like ls, cat, sed, echo, curl, python3, etc. in a Linux shell.
-    All of this runs in a docker container sandbox. So feel free to write to files, etc.
     """
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
     return result.stdout or result.stderr
