79078d62a4f510cbf1c8107b6b62963263d88f20
Greg Clinton <greg.clinton@gmail.com>
2025-02-07 15:48:20 -0800

    latest

--- tools/worker.py
+++ tools/worker.py
@@ -1,14 +1,12 @@
 import chat
 
-def reset(thread):
-    thread["workers"] = {}
+workers = {}
 
 def run(worker_name: str, prompt: str, thread: dict):
     """
     Prompts a worker with the given prompt.
     If the worker does not already exist it will be created.
     """
-    workers = thread["workers"]
 
     if worker_name not in workers:
         workers[worker_name] = chat.reset({})
