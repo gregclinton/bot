cf56ed4a27f39a2c628747606db8c0b4047ab8fd
Greg Clinton <greg.clinton@gmail.com>
2025-02-07 14:32:01 -0800

    latest

--- tools/shell.py
+++ tools/shell.py
@@ -1,6 +1,9 @@
 import subprocess
 
 def run(command: str, thread: dict):
-    "Runs the given command, like ls, cat, sed, echo, curl, python3, etc. in a Linux shell."
+    """
+    Runs the given command, like ls, cat, sed, echo, curl, python3, etc. in a Linux shell.
+    All of this runs in a docker container sandbox. So feel free to write to files, etc.
+    """
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
     return result.stdout or result.stderr
--- tools/worker.py
+++ tools/worker.py
@@ -7,17 +7,6 @@ def run(worker_name: str, prompt: str, thread: dict):
     """
     Prompts a worker with the given prompt.
     If the worker does not already exist it will be created.
-    When finished with the worker, dismiss him.
-    Use the instruct tool to keep track of your worker names and their purposes.
-
-    With workers you can divvy up a large job into many smaller jobs.
-    This helps avoid context window bloat as each worker will have its own context window.
-    workers can have their own workers or a hierarchy of workers.
-
-    You can also think of workers as specialists.
-    Divvy up a knowledge base into specialized silos and assign a worker to each one.
-
-    You yourself might be a worker and not know it. No way to know.
     """
     workers = thread["workers"]
 
