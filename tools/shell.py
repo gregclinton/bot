54893de2618e5ede2d7ad2ad86868171d4edb8a9
Greg Clinton <greg.clinton@gmail.com>
2025-03-15 15:55:16 -0700

    latest

--- tools/shell.py
+++ tools/shell.py
@@ -4,7 +4,6 @@ def run(command: str, thread: dict):
     """
     Runs the given command, like ls, cat, sed, echo, curl, python3, etc. in a Linux shell.
     Commands run in a docker container sandbox, so feel free to write to disk, etc.
-    You and each of your workers have their own working directory.
     """
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
     return result.stdout or result.stderr
