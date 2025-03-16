cb65a3dc0ce7e7404a82ea0740471c2ffa496347
Greg Clinton <greg.clinton@gmail.com>
2025-03-16 08:26:07 -0700

    latest

--- chat.py
+++ chat.py
@@ -27,5 +27,6 @@ def run(prompt, thread):
 
 if __name__ == "__main__":
     # . ./secrets
-    # run("Write a python program foo.py to output the name of a random fruit. Then run it and tell me what fruit it picked.", reset({}))
-    run("Hello. When is the Amtrak 228 due to arrive today?", reset({"user": "me", "assistant": "hal"}))
+    thread = reset({"user": "me", "assistant": "hal"})
+    # run("Write a python program foo.py to output the name of a random fruit. Then run it and tell me what fruit it picked.", thread)
+    run("Hello. When is the Amtrak 228 due to arrive today?", thread)
