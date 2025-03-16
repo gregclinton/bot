9ff69fbceb4e05ba7c615d51f561df8c15c76d44
Greg Clinton <greg.clinton@gmail.com>
2025-03-16 08:20:17 -0700

    latest

--- tools/consult.py
+++ tools/consult.py
@@ -3,11 +3,11 @@ import chat
 def reset(thread):
     thread["workers"] = {}
 
-def run(worker_name: str, prompt: str, thread: dict):
+def run(worker: str, prompt: str, thread: dict):
     "Prompts a worker with the given prompt."
     workers = thread["workers"]
 
-    if worker_name not in workers:
-        workers[worker_name] = chat.reset({"user": thread["worker"], "worker": worker_name})
+    if worker not in workers:
+        workers[worker] = chat.reset({"user": thread["worker"], "worker": worker})
 
-    return chat.run(prompt, workers[worker_name])
+    return chat.run(prompt, workers[worker])
