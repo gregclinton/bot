b13c20e29a064d8f4ed57839a2324c8817a294d8
Greg Clinton <greg.clinton@gmail.com>
2025-03-18 10:02:50 -0700

    latest

--- tools/handover.py
+++ tools/handover.py
@@ -1,6 +1,14 @@
 import chat
+import os
+
+handovers = [f for f in os.listdir("assistants") if "handover" in open(f"assistants/{f}").readline()]
 
 def run(assistant: str, thread: dict):
-    "Hands over the chat to another assistant."
+    f"Hands over the chat to another assistant. These assistants can accept a handover of the chat: {','.join(handovers)}."
+    for handover in handovers:
+        if assistant.lower() == handover.lower():
+            assistant = handover
+            break
+
     chat.handover(assistant, thread)
     return f"Hello, this is {assistant}. How can I help you?"
