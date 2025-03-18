fd91c8cc92bd61a83859645f8cc0ff6ac7f6e32c
Greg Clinton <greg.clinton@gmail.com>
2025-03-18 09:18:18 -0700

    latest

--- client/chat.js
+++ client/chat.js
@@ -54,7 +54,9 @@ const chat = {
         await chat.fetch(prompt)
         .then(response => response.text())
         .then(text => {
-            if (!'{['.includes(text[0])) {
+            if (text.startsWith('Hello, this is ')) {
+                document.title = text.split(' ')[3].split('.')[0];
+            } else if (!'{['.includes(text[0])) {
                 text = text.replace(/\\/g, '\\\\');  // so markdown won't trample LaTex
                 text = marked.parse(text)
             }
