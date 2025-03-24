3d2a1427ccc3ef4c2864b09251148795f6f1526a
Greg Clinton <greg.clinton@gmail.com>
2025-03-24 07:35:56 -0700

    latest

--- llm.py
+++ llm.py
@@ -51,6 +51,7 @@ def invoke(thread):
                 "together": "https://api.together.xyz/v1/chat/completions",
                 "groq": "https://api.groq.com/openai/v1/chat/completions",
                 "deepinfra": "https://api.deepinfra.com/v1/openai/chat/completions",
+                "nebius": "https://api.studio.nebius.com/v1/chat/completions",
             }[provider],
             headers = {
                 'Authorization': 'Bearer ' + os.environ[f"{provider.upper()}_API_KEY"],
