c11a0dbc90ee34750f38871694c0713d84114a12
Greg Clinton <greg.clinton@gmail.com>
2025-03-27 11:19:16 -0700

    latest

--- tools/handover.py
+++ tools/handover.py
@@ -1,14 +1,18 @@
 import os
 
-handovers = os.listdir("assistants")
+assistants = os.listdir("assistants")
 
 def run(assistant: str, thread: dict):
     f"""
     Hands over the chat to another assistant.
-    These assistants can accept a handover of the chat: {','.join(handovers)}.
+    These assistants can accept a handover of the chat: {','.join(assistants)}.
     Use the assistant name from this list that most closely matches the requested assistant.
-    The case for the assistant you pass this function must match the case of the name in the list.
     """
+    for name in assistants:
+        if name.lower() == assistant.lower():
+            assistant = name
+            break
+
     spec = open(f"assistants/{assistant}").read().split("\n")
     tokens = spec[0].split(' ')
     thread["provider"] = tokens[0]
