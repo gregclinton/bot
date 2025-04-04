742a243af43500af2c97ab38c0b5ae68c6ac7f78
Greg Clinton <greg.clinton@gmail.com>
2025-04-04 07:15:14 -0700

    latest

--- tools/consult.py
+++ tools/consult.py
@@ -1,6 +1,16 @@
 import chat
+import httpx
+
+is_remote = lambda name: name.startswith("https:")
 
 def reset(thread):
+    assistants = thread["assistants"]
+
+    with httpx.Client() as client:
+        for assistant in assistants.items():
+            if is_remote(assistant):
+                client.delete(f'{assistant}/threads/{id}'))
+
     thread["assistants"] = {}
 
 async def run(assistant: str, prompt: str, thread: dict):
@@ -8,6 +18,14 @@ async def run(assistant: str, prompt: str, thread: dict):
     assistants = thread["assistants"]
 
     if assistant not in assistants:
-        assistants[assistant] = chat.reset({"user": thread["assistant"], "assistant": assistant})
+        if is_remote(assistant):
+            with httpx.Client() as client:
+                assistants[assistant] = (await client.post(f'{assistant}/threads')).text
+        else:
+            assistants[assistant] = chat.reset({"user": thread["assistant"], "assistant": assistant})
 
-    return await chat.run(prompt, assistants[assistant])
+    if is_remote(assistant):
+        with httpx.Client() as client:
+            return await client.post(f'{assistant}/threads/{assistants[assistant]}/messages', content = prompt)
+    else:
+        return await chat.run(prompt, assistants[assistant])
