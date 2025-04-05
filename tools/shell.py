8c350409110e3ef4c6f065e6b6da463c90f13d9a
Greg Clinton <greg.clinton@gmail.com>
2025-04-05 08:47:09 -0700

    latest

--- tools/shell.py
+++ tools/shell.py
@@ -5,7 +5,7 @@ async def run(command: str, thread: dict):
     Runs the given command, like ls, cat, sed, echo, curl, python3, etc. in a Linux shell.
     Commands run in a docker container sandbox, so feel free to write to disk, etc.
     """
-    print(f"{thread["user"]}: shell: {command}")
+    print(f"{thread['user']}: shell: {command}")
     out, err = await (await asyncio.create_subprocess_shell(
         command,
         stdout = asyncio.subprocess.PIPE,
