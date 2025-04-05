8f7ef6f640dc54b9ad581b82a4e681623dd473eb
Greg Clinton <greg.clinton@gmail.com>
2025-04-05 11:39:28 -0700

    latest

--- bot.py
+++ bot.py
@@ -11,6 +11,9 @@ app = FastAPI(default_response_class=PlainTextResponse)
 
 threads = {}
 
+# the following three endpoints are required for a bot
+# they allow something similar to dialing a phone number, talking to the bot and then hanging up
+
 @app.post('/threads')
 async def post_thread():
     id = ''.join(random.choices(string.ascii_lowercase, k = 32))
@@ -29,6 +32,8 @@ async def delete_thread(id: str):
     if id in threads:
         await chat.reset(threads.pop(id))
     return "ok"
+
+# the remaining endpoints are just for me to play around with
 
 @app.delete('/threads/{id}/messages')
 async def delete_messages(id: str):
