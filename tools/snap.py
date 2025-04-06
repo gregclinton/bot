d8322a358b6540c269eea1de1a3dcfd538734e27
Greg Clinton <greg.clinton@gmail.com>
2025-04-06 08:14:53 -0700

    latest

--- tools/snap.py
+++ tools/snap.py
@@ -5,4 +5,5 @@ async def run(thread: dict):
     print("snap")
     chat.snapshot = thread.copy()
     chat.snapshot["messages"] = thread["messages"].copy()
+    chat.snapshot["messages"].pop() # remove the snap tool invocation message
     return "Snapshot taken."
