f044274174ba76abadf92ccfa9c2374943c31f11
Greg Clinton <greg.clinton@gmail.com>
2025-04-06 07:26:19 -0700

    latest

--- tools/bench.py
+++ tools/bench.py
@@ -3,5 +3,5 @@ import tool
 async def run(tools: str, thread: dict):
     "Provide comma separated list of tools such as: bench,consult,shell,model or 'none'."
     print(f"bench: {tools}")
-    thread["tools"] = [] if tools == "none" else tool.create(tools.split(','))
+    thread["tools"] = [] if tools == "none" else tools.split(',')
     return f"Added tools: {tools}."
