cc80dc73da105466e4c7c6763136e7cad9fb3df6
Greg Clinton <greg.clinton@gmail.com>
2025-04-07 06:51:24 -0700

    latest

--- tools/bench.py
+++ tools/bench.py
@@ -2,6 +2,6 @@ import tool
 
 async def run(tools: str, thread: dict):
     "Provide comma separated list of tools such as: bench,consult,shell,model or 'none'."
-    print(f"{thread['user']}: bench: {tools}")
+    print(f"{thread["assistant"]}: bench: {tools}")
     thread["tools"] = [] if tools == "none" else tools.split(',')
     return f"Added tools: {tools}."
--- tools/model.py
+++ tools/model.py
@@ -2,6 +2,6 @@ import chat
 
 async def run(model: str, provider: str, thread: dict):
     "Change your llm model and provider."
-    print(f"{thread['user']}: model: {model}, {provider}")
+    print(f"{thread["assistant"]}: model: {model}, {provider}")
     chat.set_model(thread, provider, model)
     return f"Now using {provider}'s {model}."
