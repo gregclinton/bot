8ba92c43d311d452a5f89242011c367a771f2455
Greg Clinton <greg.clinton@gmail.com>
2025-04-14 18:48:33 -0700

    latest

--- client/chat.js
+++ client/chat.js
@@ -121,10 +121,9 @@ window.onload = async () => {
 
     const models = document.getElementById('models');
     `
-    gpt-4.1-nano  openai
-    gpt-4.1-mini  openai
     gpt-4.1       openai
-    o3-mini       openai
+    gpt-4.1-mini  openai
+    gpt-4.1-nano  openai
     claude-3.7    anthropic claude-3-7-sonnet-latest
     gemini-2.5    google    gemini-2.5-pro-exp-03-25
     grok-3        xai
