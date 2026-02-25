51787c69f38f6a0b08611925b9f99fc2b720ef69
Greg Clinton <greg.clinton@gmail.com>
2026-02-25 11:02:22 -0800

    latest

--- hal.py
+++ hal.py
@@ -3,7 +3,7 @@ import time
 import llm
 
 while True:
-    msg = messages.get("/tmp/mail")
+    msg = messages.get("/tmp/messages/1000")
     if msg:
         print(llm.invoke("groq", "openai/gpt-oss-20b", "You are Hal.", open(msg).read()))
     time.sleep(1)
