4b7e6613a1f41a0ae360f190188d8bd708ca87c1
Greg Clinton <greg.clinton@gmail.com>
2026-02-25 11:27:11 -0800

    latest

--- hal.py
+++ hal.py
@@ -2,8 +2,17 @@ import threads
 import time
 import llm
 
+provider = "groq"
+model = "openai/gpt-oss-20b"
+
 while True:
-    thread = threads.get("/tmp/threads/1000")
+
+    thread = threads.get("1000")
     if thread:
-        print(llm.invoke("groq", "openai/gpt-oss-20b", "You are Hal.", open(thread).read()))
+        print(llm.invoke(provider, model, "You are Hal.", open(thread).read()))
+
+    thread = threads.get("1001")
+    if thread:
+        print(llm.invoke(provider, model, "You are Sally.", open(thread).read()))
+
     time.sleep(1)
--- threads.py
+++ threads.py
@@ -1,12 +1,12 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 from pathlib import Path
 
-start = datetime.now() - timedelta(days = 1)
+start = datetime.now()
 
 def get(inbox):
     global start
 
-    for p in Path(inbox).iterdir():
+    for p in Path("/tmp/threads/" + inbox).iterdir():
         if p.is_file() and datetime.fromtimestamp(p.stat().st_mtime) > start:
             start = datetime.now()
             return p
