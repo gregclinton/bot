ecfef53494ca35b528d596983f34fa21be15d185
Greg Clinton <greg.clinton@gmail.com>
2026-02-25 15:04:16 -0800

    latest

--- threads.py
+++ threads.py
@@ -5,12 +5,15 @@ def get(owner):
     # /tmp/threads/owner/correspondent/bookmark
 
     for thread in Path(f"/tmp/threads/{owner}").iterdir():
-        bookmark = f"{str(thread.resolve())}/bookmark"
+        bookmark = thread / "bookmark"
+        last = false
 
         for msg in thread.iterdir():
             if msg.name != "bookmark" and not msg.name.endswith(owner):
                 last = msg.name.split("-")[0]
 
-        if last > open(bookmark).read():
+        if last and last > open(bookmark).read():
             open(bookmark, "w").write(last)
             return "Hello."
+
+    return false
