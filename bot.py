f39c4eab7c86d17cc859e412589fe7060bfb5cf4
Greg Clinton <greg.clinton@gmail.com>
2026-02-26 10:00:15 -0800

    latest

--- bot.py
+++ bot.py
@@ -13,4 +13,14 @@ while True:
         text += f"{msg.poster} to {msg.to}: {msg.text}\n"
 
     print(text)
+
+    me = "Billing"
+    account = "CX143623"
+    text = ""
+
+    for msg in filter(lambda msg: any(account in s for s in [msg.text, msg.to, msg.poster]) or msg.poster == "Chief",  messages.mine(me)):
+        text += f"{msg.poster} to {msg.to}: {msg.text}\n"
+
+    print(text)
+
     time.sleep(1)
