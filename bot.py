321bb4707b080064c0b61d69289b598f814553af
Greg Clinton <greg.clinton@gmail.com>
2026-02-26 12:14:50 -0800

    latest

--- bot.py
+++ bot.py
@@ -2,34 +2,20 @@ import messages
 import time
 import llm
 
-def my_account_messages(me, account):
-    for m in messages.mine(me):
-        if (any(account in s for s in [m.text, m.to, m.poster]) or m.poster == "Chief"):
-            yield m
+def run_worker(worker, account):
+    invoke = lambda sys, user: llm.invoke("groq", "openai/gpt-oss-20b", sys, user)
+    text = ""
+
+    dashes = ""
+    for msg in messages.mine(worker):
+        if (any(account in s for s in [msg.text, msg.to, msg.poster]) or msg.poster == "Chief"):
+            text += f"{dashes}To: {msg.to}\nFrom: msg{msg.poster}\n{msg.text}\n"
+            dashes = "------------------------------------------------------------\n"
+        
+    print(text)
 
 while True:
-    invoke = lambda sys, user: llm.invoke("groq", "openai/gpt-oss-20b", sys, user)
     account = "CX143623"
-
-    me = "Hal"
-    text = ""
-
-    dashes = ""
-    for msg in my_account_messages(me, account):
-        text += f"{dashes}To: {msg.to}\nFrom: msg{msg.poster}\n{msg.text}\n"
-        dashes = "------------------------------------------------------------\n"
-        
-
-    print(text)
-
-    me = "Billing"
-    text = ""
-
-    dashes = ""
-    for msg in my_account_messages(me, account):
-        text += f"{msg.poster} to {msg.to}: {msg.text}\n"
-        dashes = "------------------------------------------------------------\n"
-
-    print(text)
-
-    time.sleep(1)
+    run_worker("Hal", account)
+    run_worker("Billing", account)
+    time.sleep(10)
