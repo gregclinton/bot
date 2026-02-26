1470ccf69fd1a434dfe699975d815d67b2af86f2
Greg Clinton <greg.clinton@gmail.com>
2026-02-26 09:11:42 -0800

    latest

--- bot.py
+++ bot.py
@@ -6,7 +6,11 @@ while True:
     invoke = lambda sys, user: llm.invoke("groq", "openai/gpt-oss-20b", sys, user)
 
     me = "Hal"
-    for msg in messages.mine(me):
-        print(msg.text)
-        print(invoke("", msg.text))
+    account = "CX143623"
+    text = ""
+
+    for msg in filter(lambda msg: any(account in s for s in [msg.text, msg.to, msg.poster]) or msg.poster == "Chief",  messages.mine(me)):
+        text += f"{msg.poster} to {msg.to}: {msg.text}\n"
+
+    print(text)
     time.sleep(1)
