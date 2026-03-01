e197122ccf28ad45628de7f13eeb380d032fe293
Greg Clinton <greg.clinton@gmail.com>
2026-03-01 11:48:16 -0800

    latest

--- remote.py
+++ remote.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import sys
 
 pod_id = os.environ.get("POD_ID", "ABCD")
 endpoint = f"https://{pod_id}-4000.proxy.runpod.net/messages"
@@ -10,3 +11,8 @@ def inbox(owner):
 
 def post(frm, to, body):
     requests.post(endpoint, json = { "frm": frm, "to": to, "body": body })
+
+if __name__ == "__main__":
+    name = sys.argv[1]
+    args = sys.argv[2:]
+    {"post": post}[name](*args)
