38a65c6807691e57ce702918c08ee765adfda044
Greg Clinton <greg.clinton@gmail.com>
2026-02-28 21:40:23 -0800

    latest

--- apps/wireless/tools/balance.py
+++ apps/wireless/tools/balance.py
@@ -6,4 +6,5 @@ messages = "https://example.com/messages"
 res = requests.get(f"{messages}/{worker}")
 
 for msg in res.json():
-    requests.post(f"{messages}/{msg["frm"]}", {"frm": worker, "to": msg["frm"], "body": f"{msg["body"]}\nBalance is $13.55.")
+    account = msg["body"].split(":")[1].strip()
+    requests.post(f"{messages}/{msg["frm"]}", {"frm": worker, "to": msg["frm"], "body": f"Account balance for {account} is $13.55.")
