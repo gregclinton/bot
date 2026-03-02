86f330cb2f0c23312cededa82f466df8a7f47bd4
Greg Clinton <greg.clinton@gmail.com>
2026-03-02 10:09:55 -0800

    latest

--- workers/worker.py
+++ workers/worker.py
@@ -4,11 +4,10 @@ import llm
 import re
 import storage
 import sys
+import messages
 
 workers = storage.root / "workers"
 workers.mkdir(parents = True, exist_ok = True)
-
-chief = "Above"
 
 def post(worker, text):
     for part in re.split(r'\n-{4,}\n', text.strip()):
@@ -30,25 +29,24 @@ def run(chief, worker):
         if msg.frm == chief:
             workers.add(msg.to)
 
-    for worker in workers:
-        accounts = set()
+    accounts = set()
 
-        for msg in messages.inbox(worker):
-            m = re.search(r"\bCX1\w*", f"{msg.frm} {msg.body}")
-            if m:
-                accounts.add(m.group())
+    for msg in messages.inbox(worker):
+        m = re.search(r"\bCX1\w*", f"{msg.frm} {msg.body}")
+        if m:
+            accounts.add(m.group())
 
-        for account in accounts:
-            text = ""
-            dashes = ""
-            for msg in messages.archive(worker):
-                if (any(account in s for s in [msg.body, msg.to, msg.frm]) or msg.frm == chief):
-                    t = msg.time.strftime("%A, %B %-d, %-I:%M %P")
-                    text += f"{dashes}{t}\nFrom: {msg.frm}\nTo: {msg.to}\n{msg.body}\n"
-                    dashes = "----------------------------\n"
+    for account in accounts:
+        text = ""
+        dashes = ""
+        for msg in messages.archive(worker):
+            if (any(account in s for s in [msg.body, msg.to, msg.frm]) or msg.frm == chief):
+                t = msg.time.strftime("%A, %B %-d, %-I:%M %P")
+                text += f"{dashes}{t}\nFrom: {msg.frm}\nTo: {msg.to}\n{msg.body}\n"
+                dashes = "----------------------------\n"
 
-            if text != "":
-                post(worker, llm.invoke("groq", "openai/gpt-oss-120b", "", text).strip())
+        if text != "":
+            post(worker, llm.invoke("groq", "openai/gpt-oss-120b", "", text).strip())
 
 if __name__ == "__main__":
     name = sys.argv[1]
