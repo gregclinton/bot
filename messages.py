05dbce62542f4e3be12733299dee6071cecbe914
Greg Clinton <greg.clinton@gmail.com>
2026-03-04 06:50:36 -0800

    latest

---
+++ messages.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+
+messages = Path("messages")
+messages.mkdir(exist_ok = True)
+
+app = FastAPI()
+
+@app.post("/messages/{name}")
+async def post_message(req: Request, name: str):
+    folder = messages / name
+    msg = await req.json()
+    frm, body = msg["frm"], name, msg["body"]
+    print(f"From: {frm}\nTo: {name}\n{body}\n---------------------------\n", flush = True)
+    folder.mkdir(exist_ok = True)
+    (folder / frm).write_text(body)
+    return { "status": "ok" }
+
+@app.get("/messages/{name}")
+async def get_messages(name: str):
+    folder = messages / name
+    msgs = []
+
+    if folder.exists():
+        for path in sorted(folder.iterdir(), key = lambda p: p.stat().st_mtime):
+            msgs.append({
+                "frm": path.name,
+                "to": name,
+                "body": path.read_text(),
+                timestamp: path.stat().st_mtime
+            })
+
+    return msgs
+
+app.mount("/", StaticFiles(directory = "chat", html = True))
--- messages/storage.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-# if we add a storage volume in runpod, it shows up as /workspace
-# otherwise, in development, we'll just fall back to /tmp
-
-root = Path("/workspace" if Path("/workspace").exists() else "/tmp")
