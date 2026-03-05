7f78df5d01b2b161a3cf615606055453c5e4936c
Greg Clinton <greg.clinton@gmail.com>
2026-03-05 07:36:13 -0800

    latest

--- crm/tool.py
+++ crm/tool.py
@@ -7,4 +7,5 @@ tool = sys.argv[1:]
 for msg in messages.inbox(tool):
     # will run a script with tool name
     out = subprocess.run(["sh", tool, msg.body], capture_output = True, text = True)
-    messages.post(tool, msg.frm, out.stdout + out.stderr)
+    result = out.stdout + out.stderr
+    messages.post(tool, msg.frm, result)
