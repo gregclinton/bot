c07db4a2d5d9cb4aef6d5cce4a1592f666915b4a
Greg Clinton <greg.clinton@gmail.com>
2026-03-06 08:07:06 -0800

    latest

--- crm/telegram.py
+++ crm/telegram.py
@@ -1,8 +1,8 @@
 import requests
 import os
 
-token = os.environ.get("HAL_TOKEN")
+token = os.environ.get("TELEGRAM_TOKEN")
 endpoint = f"https://api.telegram.org/bot{token}"
 
-res = requests.get(f"{endpoint}/getUpdates")
-print(res)
+for msg in requests.get(f"{endpoint}/getUpdates").json()["result"]:
+    print(msg)
