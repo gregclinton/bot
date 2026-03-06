8f5ff0d0eaa058a7aad108121f645a59446b890c
Greg Clinton <greg.clinton@gmail.com>
2026-03-06 08:29:07 -0800

    latest

--- crm/telegram.py
+++ crm/telegram.py
@@ -1,8 +1,14 @@
 import requests
 import os
+import time
 
 token = os.environ.get("TELEGRAM_TOKEN")
 endpoint = f"https://api.telegram.org/bot{token}"
+offset = 0
 
-for msg in requests.get(f"{endpoint}/getUpdates").json()["result"]:
-    print(msg)
+while True:
+    res = requests.get(f"{endpoint}/getUpdates", params = {"offset": offset}).json()
+    for update in res["result"]:
+        offset = update["update_id"] + 1
+    print(update)
+    time.sleep(1)
