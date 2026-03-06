5bbf45558e019c2f71ade86897453880d4a9ccab
Greg Clinton <greg.clinton@gmail.com>
2026-03-06 09:03:27 -0800

    latest

--- crm/telegram.py
+++ crm/telegram.py
@@ -11,6 +11,8 @@ while True:
     res = requests.get(f"{endpoint}/getUpdates", params = { "offset": offset }).json()
     for update in res["result"]:
         offset = update["update_id"] + 1
-        print(update["message"]["text"])
-        print(update)
+        message = update["message"]
+        print(message["text"])
+        requests.post(f"{endpoint}/sendMessage", json = { "chat_id": message["chat"]["id"], "text": "hello" })
     time.sleep(1)
+
