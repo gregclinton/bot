cd2ee6d762523a0e0f604e810ce7e8dc43672506
Greg Clinton <greg.clinton@gmail.com>
2026-03-06 12:26:09 -0800

    latest

--- bot/messages.py
+++ bot/messages.py
@@ -7,7 +7,8 @@ endpoint = os.environ.get("MESSAGES_URL")
 def inbox(owner):
     if owner == "Hal":
         for msg in telegram.inbox():
-            print(msg)
+            frm = msg["from"]
+            msg["from"] = f"CX1{frm}"
             yield msg
 
     res = requests.get(f"{endpoint}/{owner}")
@@ -20,6 +21,6 @@ def inbox(owner):
 
 def post(frm, to, body):
     if to.startswith("CX1"):
-        telegram.post(body)
+        telegram.post(to[3:], body)
     else:
         requests.post(endpoint, json = { "from": frm, "to": to, "body": body })
--- bot/telegram.py
+++ bot/telegram.py
@@ -7,31 +7,27 @@ import json
 token = os.environ.get("TELEGRAM_TOKEN")
 endpoint = f"https://api.telegram.org/bot{token}"
 storage = Path("telegram")
-cursor = json.loads(storage.read_text()) if storage.exists() else { "offset": 0 }
-chat_id = 0
 
 def inbox():
-    global chat_id
-    msgs = []
+    offset = int(storage.read_text()) if storage.exists() else 0
 
-    res = requests.get(f"{endpoint}/getUpdates", params = { "offset": cursor["offset"] }).json()
+    res = requests.get(f"{endpoint}/getUpdates", params = { "offset": offset }).json()
     for update in res["result"]:
-        cursor["offset"] = update["update_id"] + 1
+        offset = update["update_id"] + 1
         message = update["message"]
-        chat_id = message["chat"]["id"]
         frm = message["from"]["id"]
         body = message["text"]
         if body[0] != "/":
-            msgs.append({
+            yield {
                 "from": f"CX1{frm}",
                 "to": "Hal",
                 "body": body,
                 "timestamp": message["date"]
-            })
+            }
 
-    storage.write_text(json.dumps(cursor))
-    return msgs
+    storage.write_text(str(offset))
 
 
-def post(body):
-    requests.post(f"{endpoint}/sendMessage", json = { "chat_id": chat_id, "text": body })
+def post(to, body):
+    # except for group chats, chat id is same as user id
+    requests.post(f"{endpoint}/sendMessage", json = { "chat_id": to, "text": body })
