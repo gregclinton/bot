c7c4c8d994172aa6c1ee8a40de5ba520780c393c
Greg Clinton <greg.clinton@gmail.com>
2026-03-06 17:08:47 -0800

    latest

--- bot/messages.py
+++ bot/messages.py
@@ -1,31 +1,39 @@
 import requests
-import os
 import telegram
+from pathlib import Path
+from shutil import rmtree
 
-endpoint = os.environ.get("MESSAGES_URL")
+messages = Path("messages")
+messages.mkdir(exist_ok = True)
 
 def log(frm, to, body):
     print(f"From: {frm}\nTo: {to}\n{body}\n---------------------------\n", flush = True)
 
-def inbox(owner):
-    if owner == "Hal":
+def inbox(name):
+    if name == "Hal":
         for msg in telegram.inbox():
             frm = msg["from"]
             msg["from"] = f"TLG{frm}"
             log(msg["from"], msg["to"], msg["body"])
             yield msg
 
-    res = requests.get(f"{endpoint}/{owner}")
-    if res.ok:
-        try:
-            for msg in res.json():
-                yield msg
-        except ValueError:
-            pass
+    folder = messages / name
+
+    if folder.exists():
+        for path in sorted(folder.iterdir(), key=lambda p: p.stat().st_mtime):
+            yield {
+                "from": path.name,
+                "to": name,
+                "body": path.read_text(),
+                "timestamp": path.stat().st_mtime
+            }
+        rmtree(folder)
 
 def post(frm, to, body):
     log(frm, to, body)
     if to.startswith("TLG"):
         telegram.post(int(to[3:]), body)
     else:
-        requests.post(endpoint, json = { "from": frm, "to": to, "body": body })
+        folder = messages / to
+        folder.mkdir(exist_ok = True)
+        (folder / frm).write_text(body)
