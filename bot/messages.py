44bf9bee129a87b7f1c7f8cd103c858c87c74ab1
Greg Clinton <greg.clinton@gmail.com>
2026-03-07 10:01:56 -0800

    latest

--- bot/messages.py
+++ bot/messages.py
@@ -1,23 +1,12 @@
 import telegram
 from pathlib import Path
 from shutil import rmtree
-from threading import Thread
 
 messages = Path("messages")
 messages.mkdir(exist_ok = True)
 
 def log(frm, to, body):
     print(f"From: {frm}\nTo: {to}\n{body}\n\n---------------------------\n", flush = True)
-
-def poll_telegram():
-    while True:
-        try:
-            for msg in telegram.inbox():
-                post(f"TLG{msg['from']}", "Hal", msg["body"])
-        except:
-            pass
-
-Thread(target = poll_telegram, daemon = True).start()
 
 def inbox(name):
     folder = messages / name
--- bot/telegram.py
+++ bot/telegram.py
@@ -8,29 +8,22 @@ token = os.environ.get("TELEGRAM_TOKEN")
 endpoint = f"https://api.telegram.org/bot{token}"
 storage = Path("telegram")
 
-def inbox():
-    msgs = []
+def post(to, body):
+    # except for group chats, chat id is same as user id
+    requests.post(f"{endpoint}/sendMessage", json = { "chat_id": to, "text": body })
+
+if __name__ == "__main__":
+    import messages
+
     offset = int(storage.read_text()) if storage.exists() else 0
 
-    print("request")
+    print(f"request {offset}")
     res = requests.get(f"{endpoint}/getUpdates", params = { "timeout": 50, "offset": offset }).json()
     for update in res["result"]:
         offset = update["update_id"] + 1
         message = update["message"]
         frm = message["from"]["id"]
         body = message["text"]
-        if body[0] != "/":
-            msgs.append({
-                "from": frm,
-                "to": "Hal",
-                "body": body,
-                "timestamp": int(message["date"])
-            })
+        messages.post(f"TLG{frm}", "Hal", body)
 
     storage.write_text(str(offset))
-    return msgs
-
-
-def post(to, body):
-    # except for group chats, chat id is same as user id
-    requests.post(f"{endpoint}/sendMessage", json = { "chat_id": to, "text": body })
