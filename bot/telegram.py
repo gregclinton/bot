276c4fcda4b283d57d2a549859965e5a666eb7b5
Greg Clinton <greg.clinton@gmail.com>
2026-03-07 10:05:17 -0800

    latest

--- bot/telegram.py
+++ bot/telegram.py
@@ -6,7 +6,6 @@ import json
 # https://t.me/Hal202020Bot
 token = os.environ.get("TELEGRAM_TOKEN")
 endpoint = f"https://api.telegram.org/bot{token}"
-storage = Path("telegram")
 
 def post(to, body):
     # except for group chats, chat id is same as user id
@@ -15,15 +14,17 @@ def post(to, body):
 if __name__ == "__main__":
     import messages
 
+    storage = Path("telegram")
     offset = int(storage.read_text()) if storage.exists() else 0
 
-    print(f"request {offset}")
-    res = requests.get(f"{endpoint}/getUpdates", params = { "timeout": 50, "offset": offset }).json()
-    for update in res["result"]:
-        offset = update["update_id"] + 1
-        message = update["message"]
-        frm = message["from"]["id"]
-        body = message["text"]
-        messages.post(f"TLG{frm}", "Hal", body)
+    while True:
+        print(f"request {offset}")
+        res = requests.get(f"{endpoint}/getUpdates", params = { "timeout": 50, "offset": offset }).json()
+        for update in res["result"]:
+            offset = update["update_id"] + 1
+            message = update["message"]
+            frm = message["from"]["id"]
+            body = message["text"]
+            messages.post(f"TLG{frm}", "Hal", body)
 
-    storage.write_text(str(offset))
+        storage.write_text(str(offset))
