07b590c0019666048603902837e8f5edf3a397b0
Greg Clinton <greg.clinton@gmail.com>
2026-03-08 18:39:46 -0700

    latest

--- worker.py
+++ worker.py
@@ -58,7 +58,7 @@ for account in incoming_accounts:
         timestamp = float(timestamp)
         time = datetime.fromtimestamp(timestamp).strftime("%A, %B %-d, %-I:%M %P")
         body = path.read_text()
-        text += f"{time}\nFrom: {frm}\nTo: {to}\n{body}\n----------------\n"
+        text += f"{time}\nFrom: {frm}\nTo: {to}\n{body}\n\n\n\"
 
     response = llm.invoke(llm_provider, llm_model, "", text).strip() if text else ""
 
