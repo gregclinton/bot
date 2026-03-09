a49bb4a513a5acf5aaca7428a1169aa5f380c382
Greg Clinton <greg.clinton@gmail.com>
2026-03-09 12:21:18 -0700

    latest

--- bot/tool.py
+++ bot/tool.py
@@ -5,10 +5,8 @@ import re
 
 tool = sys.argv[1]
 
-for frm, to, body, timestamp in messages.inbox(tool):
+for frm, to, account, timestamp in messages.inbox(tool):
     # will run a script with tool name
     out = subprocess.run(["sh", tool, body], capture_output = True, text = True)
     result = out.stdout + out.stderr
-    m = re.search(r"\bTLG\w*", f"{body}")
-    result = f"Account: {m.group()}\n{result}" if m else result
-    messages.post(tool, frm, result)
+    messages.post(tool, frm, account, result)
