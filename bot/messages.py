0646a8e3d7907ce9c9139c9dff14e0a70bf493e6
Greg Clinton <greg.clinton@gmail.com>
2026-03-09 14:48:46 -0700

    latest

--- bot/messages.py
+++ bot/messages.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from shutil import rmtree
 
-# messages/to/from|order  body
+# messages/to/account/from|order  body
 
 messages = Path("messages")
 messages.mkdir(exist_ok = True)
@@ -13,18 +13,22 @@ def inbox(name):
     folder = messages / name
 
     if folder.exists():
-        for path in sorted(folder.iterdir(), key=lambda p: p.stat().st_mtime):
-            frm, text, timestamp = path.name.split("|")[0], path.read_text(), int(path.stat().st_mtime)
-            lines = text.split("\n")
-            account = lines[0].split(":")[1].strip()
-            body = "\n".join(lines[1:])
+        paths = []
+
+        for account in folder.iterdir():
+            for path in account.iterdir():
+                paths.append(path)
+
+        for path in sorted(paths, key = lambda p: p.stat().st_mtime):
+            frm = path.name.split("|")[0]
+            account, body, timestamp = path.parent.name, path.read_text(), int(path.stat().st_mtime)
             yield frm, account, body, timestamp
+
         rmtree(folder)
 
 def post(frm, to, account, body):
     log(frm, to, account, body)
-    folder = messages / to
-    folder.mkdir(exist_ok = True)
+    folder = messages / to / account
+    folder.mkdir(parents = True, exist_ok = True)
     order = len(list(folder.glob((folder / frm).name + "*")))
-    body = body if body.startswith("Account:") else f"Account: {account}\n{body}"
     (folder / f"{frm}|{order + 1:06d}").write_text(body)
