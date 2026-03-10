c4872f1306c751df646296052f71d8fea75795f1
Greg Clinton <greg.clinton@gmail.com>
2026-03-10 08:09:46 -0700

    latest

--- bot/tool.py
+++ bot/tool.py
@@ -7,7 +7,7 @@ tool = sys.argv[1]
 
 for frm, account, body, timestamp in messages.inbox(tool):
     # will run a script with tool name
-    out = subprocess.run(["sh", tool, body], capture_output = True, text = True)
+    out = subprocess.run(["sh", tool, account, body], capture_output = True, text = True)
     result = out.stdout + out.stderr
     print(f"{tool}:\n{result.strip()}\n")  
     messages.post(tool, frm, account, result)
