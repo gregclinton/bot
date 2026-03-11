c7b9c1620a450873ec8622707d9989342c8e419d
Greg Clinton <greg.clinton@gmail.com>
2026-03-11 10:07:07 -0700

    latest

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+# docker build -t gregclinton/hal .
+# docker login -u gregclinton
+# docker push gregclinton/hal
+
+FROM python:3.11-slim
+RUN apt-get update && apt-get install -y curl
+RUN pip install requests fastapi uvicorn
+WORKDIR /root
+COPY start .
+CMD ["sh", "start"]
---
+++ chat/chat.js
@@ -0,0 +1,59 @@
+document.title = 'hal';
+
+const chat = {
+    account: 'CX143623',
+
+    correspondent: "Hal",
+
+    post: (name, text) => {
+        const title = document.createElement('span');
+
+        title.innerHTML = name;
+        title.classList.add('name');
+
+        const top = document.createElement('div');
+
+        top.append(title);
+
+        const bottom = document.createElement('div');
+
+        const post = document.createElement('div');
+        post.append(top, bottom);
+        post.classList.add('post');
+        document.getElementById('chat').appendChild(post);
+
+        bottom.innerHTML = text;
+
+        post.scrollIntoView({ behavior: 'smooth' });
+    },
+
+    prompt: async () => {
+        const e = document.getElementById("prompt")
+        const prompt = e.value.trim()
+        chat.post("me", prompt);
+        e.value = '';
+        fetch(`/messages/${chat.correspondent}`, {
+            method: 'POST',
+            headers:  { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ frm: chat.account, body: prompt })
+        });
+    },
+
+    clear: () => {
+        document.getElementById('chat').innerHTML = '';
+    },
+
+    run: () => {
+        fetch(`/messages/${chat.account}`)
+        .then(res => res.json())
+        .then(list => {
+            chat.correspondent = msg.frm;
+            list.forEach(msg => chat.post(msg.frm, marked.parse(msg.body)));
+            setTimeout(chat.run, 1000);
+        });
+    }
+};
+
+window.onload = async () => {
+    chat.run();
+};
