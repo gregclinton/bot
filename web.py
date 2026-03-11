439493535694f372f335ea57b6fa80cccf16ecae
Greg Clinton <greg.clinton@gmail.com>
2026-03-11 11:29:19 -0700

    latest

--- web.py
+++ web.py
@@ -12,17 +12,16 @@ async def post_message(req: Request):
     return "ok"
 
 @app.get("/messages/{to}")
-async def get_messages(owner: str, timeout: int = 10):
+async def get_messages(to: str, timeout: int = 10):
     results = []
     start = time.time()
 
     while True:
-        for frm, to, body, timestamp in messages.inbox(to):
+        for _, _, body, _ in messages.inbox(to):
             results.append(body)
         if results or time.time() - start > timeout:
             break
-        else:
-            time.sleep(0.2)
+        await asyncio.sleep(0.2)
 
     return results
 
