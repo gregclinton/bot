d1241c85cbab81f31ee0a1c659ccfffa30398157
Greg Clinton <greg.clinton@gmail.com>
2026-03-11 11:24:30 -0700

    latest

--- web.py
+++ web.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 import messages
+import time
 
 app = FastAPI()
 
@@ -10,8 +11,19 @@ async def post_message(req: Request):
     messages.post(msg["frm"], msg["to"], msg["body"])
     return "ok"
 
-@app.get("/messages/{owner}")
-async def get_messages(owner: str):
-    return list(messages.inbox(owner))
+@app.get("/messages/{to}")
+async def get_messages(owner: str, timeout: int = 10):
+    results = []
+    start = time.time()
+
+    while True:
+        for frm, to, body, timestamp in messages.inbox(to):
+            results.append(body)
+        if results or time.time() - start > timeout:
+            break
+        else:
+            time.sleep(0.2)
+
+    return results
 
 app.mount("/", StaticFiles(directory = "chat", html = True))
