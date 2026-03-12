2d0a7a888e2960d8e5135f51e147ff76ec910f47
Greg Clinton <greg.clinton@gmail.com>
2026-03-11 18:22:11 -0700

    latest

--- bot/tool.py
+++ bot/tool.py
@@ -8,6 +8,6 @@ tool = sys.argv[1]
 for frm, body, timestamp in messages.inbox(tool):
     # will run a script with tool name
     out = subprocess.run(["sh", tool, body], capture_output = True, text = True)
-    result = out.stdout + out.stderr
-    print(f"From: {tool}\nTo: {frm}\n{result.strip()}\n")  
+    result = (out.stdout + out.stderr).strip()
+    print(f"From: {tool}\nTo: {frm}\n{result}\n")  
     messages.post(tool, frm, result)
--- bot/worker.py
+++ bot/worker.py
@@ -31,7 +31,7 @@ def post(to, account, body):
 
 for frm, body, timestamp in messages.inbox(worker):
     if frm.startswith("TLG"):
-        print(f"From: Customer\nTo: {worker}\n{body}\n")
+        print(f"From: {frm}\nTo: {worker}\n{body}\n")
 
     last_timestamp = timestamp
     m = re.search(r"\bTLG\w*", f"{frm} {body}")
@@ -49,8 +49,7 @@ for account in incoming_accounts:
     for path in sorted(all_msgs, key = lambda p: float(p.name.split("|")[0])):
         timestamp, frm, to = path.name.split("|")
         body = path.read_text()
-        anonymize = lambda id: "Customer" if id.startswith("TLG") else id
-        text += f"\nFrom: {anonymize(frm)}\nTo: {anonymize(to)}\n{body}\n"
+        text += f"\nFrom: {frm}\nTo: {to}\n{body}\n"
 
     text += f"\nFrom: {worker}"
     response = llm.invoke(llm_provider, llm_model, "", text).strip() if text else ""
@@ -62,7 +61,6 @@ for account in incoming_accounts:
         if line.startswith("To:"):
             post(to, account, body)
             to = line.split(':')[1].strip()
-            to = account if to == "Customer" else to
             body = ""
         else:
             body += f"{line}\n"
