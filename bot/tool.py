abcd6e7bf69edb941b7f9f2d2d85760f3a4e475a
Greg Clinton <greg.clinton@gmail.com>
2026-03-13 12:20:35 -0700

    latest

--- bot/tool.py
+++ bot/tool.py
@@ -5,14 +5,17 @@ import re
 
 tool = sys.argv[1]
 
-for frm, body, timestamp in messages.inbox(tool): 
+for frm, body, timestamp in messages.inbox(tool):
     m = re.search(r"\bTLG\w*", f"{frm} {body}")
     if m:
         account = m.group()
 
-        # will run a script with tool 
+        # will run a script with tool
         out = subprocess.run(["sh", tool, account, body], capture_output = True, text = True)
         result = (out.stdout + out.stderr).strip()
-        result = (result if account in result else f"In reference to account: {account}\n{result}")
-        print(f"From: {tool}\nTo: {frm}\n{result}\n")  
+
+        if account not in result:
+            result = f"In reference to account: {account}\n{result}"
+
+        print(f"From: {tool}\nTo: {frm}\n{result}\n")
         messages.post(tool, frm, result)
