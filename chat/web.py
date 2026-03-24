411e76bc030bd7b2309d126cd31cc001252e6007
Greg Clinton <greg.clinton@gmail.com>
2026-03-24 11:25:06 -0700

    latest

--- chat/web.py
+++ chat/web.py
@@ -18,7 +18,7 @@ async def get_messages(to: str, timeout: int = 10):
     start = time.time()
 
     while True:
-        for frm, body, timestamp in messages.inbox(to):
+        for frm, body, timestamp in messages.inbox(to, 0):
             results.append({"from": frm, "body": body, "timestamp": timestamp})
         if results or time.time() - start > timeout:
             break
