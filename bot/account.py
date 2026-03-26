c656493c3ee83ba96e68f666335889ecf60bb039
Greg Clinton <greg.clinton@gmail.com>
2026-03-26 13:24:34 -0700

    latest

--- bot/account.py
+++ bot/account.py
@@ -1,5 +1,22 @@
 import re
+from random import randint
+from pathlib import Path
+
+accounts = Path("accounts")
 
 def scrape(text):
+    # you should implement this yourself
     m = re.search(r"\bTLG\w*", text)
     return m.group() if m else None
+
+def get(session):
+    # you should implement this yourself
+    path = accounts / session
+
+    if path.exists():
+        account = path.read_text()
+    else:
+        account = f"TLG{10000000 + randint(1, 9999999)}"
+        path.write_text(account)
+
+    return account
--- chat/web.py
+++ chat/web.py
@@ -4,19 +4,15 @@ import messages
 import time
 import asyncio
 import secrets
+import account
 
 app = FastAPI()
 
-def get_account(session):
-    # for now -- your system should implement
-    return "TLG143623" if session else None
-
 @app.post('/messages')
-async def post_message(req: Request):
+async def post_message(req: Request, session: str = Cookie(None)):
     msg = await req.json()
-    account = get_account(req.cookies.get("session"))
-    if account:
-        messages.post(account, "", msg["body"])
+    acct = account.get(session)
+    messages.post(acct, "", msg["body"])
     return "ok"
 
 @app.get("/messages")
@@ -29,10 +25,10 @@ async def get_messages(response: Response, after: float, session: str = Cookie(N
         session = secrets.token_hex(16)
         response.set_cookie(key="session", value = session, httponly = True)
 
-    account = get_account(session)
-    if account:
+    acct = account.get(session)
+    if acct:
         while not posts and time.time() - start < 60:
-            for frm, body, timestamp in messages.chat(account, after):
+            for frm, body, timestamp in messages.chat(acct, after):
                 posts.append({"from": frm, "body": body, "timestamp": timestamp})
             await asyncio.sleep(0.2)
 
